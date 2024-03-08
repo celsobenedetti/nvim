30db2e4da9a3464e8f0f427cbf36bee2299a5061
Celso Benedetti <celsobenedetti2@gmail.com>
2024-03-08 18:25:38 -0300

    feat: harpoon

---
+++ lua/plugins/harpoon.lua
@@ -0,0 +1,61 @@
+return {
+  'ThePrimeagen/harpoon',
+  branch = 'harpoon2',
+  opts = {
+    menu = {
+      width = vim.api.nvim_win_get_width(0) - 4,
+    },
+  },
+  keys = {
+    {
+      '<leader>m',
+      function()
+        require('harpoon'):list():append()
+      end,
+      desc = 'Harpoon file',
+    },
+    {
+      'mm',
+      function()
+        local harpoon = require 'harpoon'
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end,
+      desc = 'Harpoon quick menu',
+    },
+    {
+      'ma',
+      function()
+        require('harpoon'):list():select(1)
+      end,
+      desc = 'Harpoon to file 1',
+    },
+    {
+      'ms',
+      function()
+        require('harpoon'):list():select(2)
+      end,
+      desc = 'Harpoon to file 2',
+    },
+    {
+      'md',
+      function()
+        require('harpoon'):list():select(3)
+      end,
+      desc = 'Harpoon to file 3',
+    },
+    {
+      'mf',
+      function()
+        require('harpoon'):list():select(4)
+      end,
+      desc = 'Harpoon to file 4',
+    },
+    {
+      'mg',
+      function()
+        require('harpoon'):list():select(5)
+      end,
+      desc = 'Harpoon to file 5',
+    },
+  },
+}
