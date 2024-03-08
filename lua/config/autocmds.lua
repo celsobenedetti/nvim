80ec4a16a7cda7e566b06a144b500bdd9801d6e5
Celso Benedetti <celsobenedetti2@gmail.com>
2024-03-08 18:31:13 -0300

    feat: ui + autocmds

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,8 +1,57 @@
--- Highlight when yanking (copying) text
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
+local all = nil -- vim.api.nvim_create_augroup('AllFilesGroup', { clear = true })
+local markdown = vim.api.nvim_create_augroup('MarkdownGroup', { clear = true })
+local json = vim.api.nvim_create_augroup('JSONGroup', { clear = true })
+local new_note = vim.api.nvim_create_augroup('NewNoteGroup', { clear = true })
+
+-- All files --------------------------------------------------------------
+
+if all then
+  vim.api.nvim_create_autocmd({ 'VimEnter' }, {
+    callback = function()
+      vim.cmd 'setlocal formatoptions-=cro' -- Stop comment continuation on line below
+      vim.cmd 'hi SpellBad guifg=#d8dee9'
+      -- vim.cmd("TSDisable highlight") -- Disable TreeSitter highlighting
+    end,
+    group = all,
+    desc = 'Run on all files',
+  })
+end
+
+-- Markdown --------------------------------------------------------------
+
+if markdown then
+  vim.api.nvim_create_autocmd({ 'VimEnter' }, {
+    pattern = '*.md',
+    callback = function()
+      require('twilight').enable()
+    end,
+    group = markdown,
+    desc = 'Run when entering vim in Markdown file',
+  })
+end
+
+-- JSON --------------------------------------------------------------
+
+if json then
+  vim.api.nvim_create_autocmd({ 'VimEnter' }, {
+    pattern = '*.json',
+    callback = function()
+      vim.o.conceallevel = 3
+    end,
+    group = json,
+    desc = 'Run when entering JSON files',
+  })
+end
+
+-- new_note --------------------------------------------------------------
+
+if new_note then
+  vim.api.nvim_create_autocmd({ 'VimEnter' }, {
+    pattern = 'title_editor',
+    callback = function()
+      vim.api.nvim_feedkeys(Escape 'ggi', 'n', true)
+    end,
+    group = new_note,
+    desc = 'Run when entering new_note files',
+  })
+end
---
+++ lua/plugins/ui.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    'folke/twilight.nvim',
+    config = function()
+      require('twilight').setup {
+        context = 20, -- amount of lines we will try to show around the current line
+      }
+    end,
+    ft = { 'markdown' },
+    keys = { { '<leader>tw', ':Twilight<CR>' } },
+  },
+
+  -- filenamees
+  {
+    'b0o/incline.nvim',
+    opts = function(_, opts)
+      opts.window = { margin = { vertical = 0, horizontal = 1 } }
+      opts.hide = { cursorline = true }
+
+      opts.render = function(props)
+        local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ':t')
+        if vim.bo[props.buf].modified then
+          filename = '[+] ' .. filename
+        end
+
+        local icon, color = require('nvim-web-devicons').get_icon_color(filename)
+        return { { icon, guifg = color }, { ' ' }, { filename } }
+      end
+    end,
+  },
+}
