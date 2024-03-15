4dfd9ab1a3d4879f55fa41dfa73b5b7a454a2a53
Celso Benedetti <celsobenedetti2@gmail.com>
2024-03-15 12:55:06 -0300

    feat(core): lint.lua

---
+++ lua/c/core/plugins/lint.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    'mfussenegger/nvim-lint',
+    event = 'VeryLazy',
+    config = function()
+      require('lint').linters_by_ft = {
+        markdown = { 'vale' },
+        bash = { 'shellcheck' },
+        sh = { 'shellcheck' },
+      }
+
+      vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
+        callback = function()
+          require('lint').try_lint()
+        end,
+        group = vim.api.nvim_create_augroup('nvim-lint-bufwritepost', { clear = true }),
+      })
+    end,
+  },
+}
