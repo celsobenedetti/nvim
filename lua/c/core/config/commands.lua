3bb933cf28ceaac5e248b5de88705f59793ad950
Celso Benedetti <celsobenedetti2@gmail.com>
2024-06-03 18:46:22 -0300

    feat: Jira issue command

--- lua/c/core/config/commands.lua
+++ lua/c/core/config/commands.lua
@@ -2,3 +2,8 @@ local tmux_window = '!tmux neww '
 
 vim.api.nvim_create_user_command('Clip', '!echo % | xclip -sel clip', {})
 vim.api.nvim_create_user_command('Note', tmux_window .. 'note.sh', {})
+
+vim.api.nvim_create_user_command('Is', function(args)
+  local jira = 'https://ocelotbot.atlassian.net/browse/'
+  os.execute('xdg-open ' .. jira .. args.args)
+end, { nargs = 1 })
