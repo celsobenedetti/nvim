2cefbfd9c7d1e3db7f622f0fd3c1e2e5aabef0c7
Celso Benedetti <celsobenedetti2@gmail.com>
2024-06-05 18:52:24 -0300

    feat: resize nvim on window size change autocmd 👑
    https://www.reddit.com/r/neovim/comments/vnodft/nvimtmux_resize_vim_split_automatically/

--- lua/c/core/config/autocmds.lua
+++ lua/c/core/config/autocmds.lua
@@ -48,3 +48,14 @@ vim.api.nvim_create_autocmd('VimEnter', {
     vim.api.nvim_feedkeys(Keys 'i<BS>', 'n', true)
   end,
 })
+
+-- tmux
+
+local wr_group = vim.api.nvim_create_augroup('WinResize', { clear = true })
+
+vim.api.nvim_create_autocmd('VimResized', {
+  group = wr_group,
+  pattern = '*',
+  command = 'wincmd =',
+  desc = 'Automatically resize windows when the host window size changes.',
+})
