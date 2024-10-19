ce18a8262011f92f29e2a7c4f9ec137c55aa4750
Celso Benedetti <celsobenedetti2@gmail.com>
2024-10-19 13:10:01 -0300

    feat: deno lint and format

--- lua/c/core/plugins/conform.lua
+++ lua/c/core/plugins/conform.lua
@@ -1,42 +1,48 @@
 return {
+
   { -- Autoformat
     'stevearc/conform.nvim',
-    opts = {
-      log_level = vim.log.levels.WARN,
-      notify_on_error = false,
-      format_on_save = function()
-        return {
-          timeout_ms = 5000,
-          lsp_fallback = true,
-          filter = function(client)
-            return vim.g.autoformat and client.name ~= 'tsserver'
-          end,
-        }
-      end,
-      formatters_by_ft = {
-        go = { 'goimports' },
-        lua = { 'stylua' },
-        python = { 'isort', 'black' },
-        markdown = { 'markdownlint-cli2' },
+    opts = function()
+      local is_deno = require('lspconfig.util').root_pattern('deno.json', 'deno.jsonc')
+      local js_formatter = is_deno and 'deno' or 'prettierd'
 
-        typescript = { 'prettierd' },
-        javascript = { 'prettierd' },
-        javascriptreact = { 'prettierd' },
-        typescriptreact = { 'prettierd' },
+      return {
+        log_level = vim.log.levels.WARN,
+        notify_on_error = false,
+        format_on_save = function()
+          return {
+            timeout_ms = 5000,
+            lsp_fallback = true,
+            filter = function(client)
+              return vim.g.autoformat and client.name ~= 'tsserver'
+            end,
+          }
+        end,
+        formatters_by_ft = {
+          go = { 'goimports' },
+          lua = { 'stylua' },
+          python = { 'isort', 'black' },
+          markdown = { 'markdownlint-cli2' },
 
-        graphql = { 'prettierd' },
-        json = { 'prettierd' },
-        toml = { 'taplo' },
+          typescript = { js_formatter },
+          javascript = { js_formatter },
+          javascriptreact = { js_formatter },
+          typescriptreact = { js_formatter },
 
-        sh = { 'shfmt' },
-        bash = { 'shfmt' },
-        zsh = { 'shfmt' },
-      },
-      formatters = {
-        goimports = {
-          prepend_args = { '-local', 'github.com/celsobenedetti/' },
+          graphql = { js_formatter },
+          json = { js_formatter },
+          toml = { 'taplo' },
+
+          sh = { 'shfmt' },
+          bash = { 'shfmt' },
+          zsh = { 'shfmt' },
         },
-      },
-    },
+        formatters = {
+          goimports = {
+            prepend_args = { '-local', 'github.com/celsobenedetti/' },
+          },
+        },
+      }
+    end,
   },
 }
--- lua/c/core/plugins/lint.lua
+++ lua/c/core/plugins/lint.lua
@@ -3,11 +3,26 @@ return {
     'mfussenegger/nvim-lint',
     event = 'VeryLazy',
     config = function()
+      local is_deno = require('lspconfig.util').root_pattern('deno.json', 'deno.jsonc')
+      local js_linter = is_deno and 'deno' or 'prettierd'
+
       require('lint').linters_by_ft = {
         bash = { 'shellcheck' },
         sh = { 'shellcheck' },
         make = { 'checkmake' },
+        typescript = { js_linter },
       }
+      -- Show linters for the current buffer's file type
+      vim.api.nvim_create_user_command('LintInfo', function()
+        local filetype = vim.bo.filetype
+        local linters = require('lint').linters_by_ft[filetype]
+
+        if linters then
+          print('Linters for ' .. filetype .. ': ' .. table.concat(linters, ', '))
+        else
+          print('No linters configured for filetype: ' .. filetype)
+        end
+      end, {})
 
       vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
         callback = function()
