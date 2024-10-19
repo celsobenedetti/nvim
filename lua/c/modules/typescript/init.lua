39c298b9353059e4c80bcbf1667f5cd445a4fced
Celso Benedetti <celsobenedetti2@gmail.com>
2024-10-19 12:58:45 -0300

    remove: typescript-tools.nvim

--- lua/c/modules/typescript/init.lua
+++ lua/c/modules/typescript/init.lua
@@ -2,10 +2,10 @@ local ft = { 'typescript', 'typescriptreact', 'javascript', 'javascriptreact' }
 
 return {
   { 'dmmulroy/ts-error-translator.nvim', ft = ft, config = true },
-  {
-    'pmizio/typescript-tools.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
-    opts = {},
-    ft = ft,
-  },
+  -- {
+  --   'pmizio/typescript-tools.nvim',
+  --   dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
+  --   opts = {},
+  --   ft = ft,
+  -- },
 }
