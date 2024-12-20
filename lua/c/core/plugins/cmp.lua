8af67e05c83dc3e7180653b200d7dfea3e66851b
Celso Benedetti <celsobenedetti2@gmail.com>
2024-12-19 21:33:59 -0300

    wip: snippets shenanigans

--- lua/c/core/plugins/cmp.lua
+++ lua/c/core/plugins/cmp.lua
@@ -8,9 +8,13 @@ return {
       },
     },
     config = function()
-      require('luasnip.loaders.from_vscode').lazy_load {}
-      -- custom snippets
-      require('luasnip.loaders.from_vscode').lazy_load { paths = { '~/.config/nvim/snippets/' } }
+      -- require('luasnip.loaders.from_vscode').lazy_load {}
+      require('luasnip.loaders.from_vscode').lazy_load {
+        paths = {
+          '/home/celso/.config/nvim',
+          -- '/home/celso/.local/share/nvim/lazy/friendly-snippets',
+        },
+      }
     end,
   },
   { -- Autocompletion
