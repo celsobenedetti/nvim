a3ad795d5d64bc1db0efef79fdd25df6c6fa7180
Celso Benedetti <celsobenedetti2@gmail.com>
2025-01-01 20:12:45 -0300

    feat: replace incline.nvim for bufferline

---
+++ lua/c/modules/ui/bufferline.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    'akinsho/bufferline.nvim',
+    version = '*',
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    opts = true,
+    keys = {
+      { '<leader>bp', '<Cmd>BufferLineTogglePin<CR>', desc = 'Bufferline: Toggle Pin' },
+      { '<leader>bP', '<Cmd>BufferLineGroupClose ungrouped<CR>', desc = 'Bufferline: Delete Non-Pinned Buffers' },
+      { '<leader>br', '<Cmd>BufferLineCloseRight<CR>', desc = 'Bufferline: Delete Buffers to the Right' },
+      { '<leader>bl', '<Cmd>BufferLineCloseLeft<CR>', desc = 'Bufferline: Delete Buffers to the Left' },
+      { '<S-h>', '<cmd>BufferLineCyclePrev<cr>', desc = 'Bufferline: Prev Buffer' },
+      { '<S-l>', '<cmd>BufferLineCycleNext<cr>', desc = 'Bufferline: Next Buffer' },
+      { '[b', '<cmd>BufferLineCyclePrev<cr>', desc = 'Bufferline: Prev Buffer' },
+      { ']b', '<cmd>BufferLineCycleNext<cr>', desc = 'Bufferline: Next Buffer' },
+      { '[B', '<cmd>BufferLineMovePrev<cr>', desc = 'Bufferline: Move buffer prev' },
+      { ']B', '<cmd>BufferLineMoveNext<cr>', desc = 'Bufferline: Move buffer next' },
+    },
+  },
+}
--- lua/c/modules/ui/ui.lua
+++ lua/c/modules/ui/ui.lua
@@ -11,26 +11,6 @@ return {
     keys = { { '<leader>tw', ':Twilight<CR>' } },
   },
 
-  -- filenamees
-  -- TODO: is this needed? Maybe tabs would be better
-  {
-    'b0o/incline.nvim',
-    opts = function(_, opts)
-      opts.window = { margin = { vertical = 0, horizontal = 1 } }
-      opts.hide = { cursorline = true }
-
-      opts.render = function(props)
-        local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ':t')
-        if vim.bo[props.buf].modified then
-          filename = '[+] ' .. filename
-        end
-
-        local icon, color = require('nvim-web-devicons').get_icon_color(filename)
-        return { { icon, guifg = color }, { ' ' }, { filename } }
-      end
-    end,
-  },
-
   {
     'levouh/tint.nvim',
     opts = true,
