9a1945566bc3700c2925e8b0ea1cd82058b746c0
Celso Benedetti <celsobenedetti2@gmail.com>
2025-01-02 10:04:14 -0300

    add: WA Wa cmds

--- lua/c/core/config/commands.lua
+++ lua/c/core/config/commands.lua
@@ -6,3 +6,8 @@ vim.api.nvim_create_user_command('Note', tmux_window .. 'note.sh', {})
 vim.api.nvim_create_user_command('Is', function(args)
   require('c.functions.issues').open_issue(args.args)
 end, { nargs = 1 })
+
+-- stylua: ignore
+local write_buffer = function() vim.cmd 'wa' end
+vim.api.nvim_create_user_command('Wa', write_buffer, { nargs = 0 })
+vim.api.nvim_create_user_command('WA', write_buffer, { nargs = 0 })
