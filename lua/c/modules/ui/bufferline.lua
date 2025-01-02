4eb4fcd2a183d98e1fd5c3bf791c3feec8a0493e
Celso Benedetti <celsobenedetti2@gmail.com>
2025-01-02 11:29:22 -0300

    fix: snacks indent highlight only on current window

--- lua/c/modules/ui/bufferline.lua
+++ lua/c/modules/ui/bufferline.lua
@@ -1,9 +1,12 @@
 return {
   {
     'akinsho/bufferline.nvim',
+    event = 'VeryLazy',
     version = '*',
     dependencies = 'nvim-tree/nvim-web-devicons',
-    opts = true,
+    config = function()
+      require('bufferline').setup()
+    end,
     keys = {
       { '<leader>bp', '<Cmd>BufferLineTogglePin<CR>', desc = 'Bufferline: Toggle Pin' },
       { '<leader>bP', '<Cmd>BufferLineGroupClose ungrouped<CR>', desc = 'Bufferline: Delete Non-Pinned Buffers' },
--- lua/c/modules/ui/ui.lua
+++ lua/c/modules/ui/ui.lua
@@ -17,18 +17,6 @@ return {
   },
 
   {
-    'lukas-reineke/indent-blankline.nvim',
-    main = 'ibl',
-    opts = {
-      indent = {
-        char = '│',
-        tab_char = '│',
-      },
-      scope = { enabled = false, show_start = false, show_end = false },
-    },
-  },
-
-  {
     'norcalli/nvim-colorizer.lua',
     config = {
       'toml',
