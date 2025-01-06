c16e942710ee852ae2712d25061c9c6ef728e593
Celso Benedetti <celsobenedetti2@gmail.com>
2025-01-05 21:10:47 -0300

    ui: noice command and options

--- lua/c/modules/ui/noice.lua
+++ lua/c/modules/ui/noice.lua
@@ -40,6 +40,36 @@ return {
         long_message_to_split = true,
         lsp_doc_border = true, -- add a border to hover docs and signature help
       },
+      views = {
+        cmdline_popup = {
+          position = {
+            row = '40%',
+            col = '50%',
+          },
+          size = {
+            width = 60,
+            height = 'auto',
+          },
+        },
+        popupmenu = {
+          relative = 'editor',
+          position = {
+            row = '52%',
+            col = '50%',
+          },
+          size = {
+            width = 60,
+            height = 10,
+          },
+          border = {
+            style = 'rounded',
+            padding = { 0, 1 },
+          },
+          win_options = {
+            winhighlight = { Normal = 'Normal', FloatBorder = 'DiagnosticInfo' },
+          },
+        },
+      },
     },
     -- stylua: ignore
     keys = {
