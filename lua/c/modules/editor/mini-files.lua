e9596e6e315eae03916efb64f3bcab3244c5da18
Celso Benedetti <celsobenedetti2@gmail.com>
2025-01-08 14:55:12 -0300

    feat: mini.files splits

--- lua/c/modules/editor/mini-files.lua
+++ lua/c/modules/editor/mini-files.lua
@@ -12,24 +12,58 @@ return {
     'echasnovski/mini.files',
     version = '*',
     lazy = true,
-    opts = {
-      mappings = {
-        close = 'q',
-        go_in = 'L',
-        go_out = 'H',
-        go_in_plus = '<C-l>',
-        go_out_plus = '<C-h>',
-        reset = '<BS>',
-        reveal_cwd = '@',
-        show_help = 'g?',
-        synchronize = '<C-y>',
-        trim_left = '<',
-        trim_right = '>',
-      },
-      options = {
-        use_as_default_explorer = true,
-      },
-    },
+    config = function()
+      local MiniFiles = require 'mini.files'
+      MiniFiles.setup {
+        mappings = {
+          close = 'q',
+          go_in = 'L',
+          go_out = 'H',
+          go_in_plus = '<C-l>',
+          go_out_plus = '<C-h>',
+          reset = '<BS>',
+          reveal_cwd = '@',
+          show_help = 'g?',
+          synchronize = '<C-y>',
+          trim_left = '<',
+          trim_right = '>',
+        },
+        options = {
+          use_as_default_explorer = true,
+        },
+      }
+
+      local map_split = function(buf_id, lhs, direction)
+        local rhs = function()
+          -- Make new window and set it as target
+          local cur_target = MiniFiles.get_explorer_state().target_window
+          local new_target = vim.api.nvim_win_call(cur_target, function()
+            vim.cmd(direction .. ' split')
+            return vim.api.nvim_get_current_win()
+          end)
+
+          MiniFiles.set_target_window(new_target)
+
+          -- This intentionally doesn't act on file under cursor in favor of
+          -- explicit "go in" action (`l` / `L`). To immediately open file,
+          -- add appropriate `MiniFiles.go_in()` call instead of this comment.
+        end
+
+        -- Adding `desc` will result into `show_help` entries
+        local desc = 'Split ' .. direction
+        vim.keymap.set('n', lhs, rhs, { buffer = buf_id, desc = desc })
+      end
+
+      vim.api.nvim_create_autocmd('User', {
+        pattern = 'MiniFilesBufferCreate',
+        callback = function(args)
+          local buf_id = args.data.buf_id
+          -- Tweak keys to your liking
+          map_split(buf_id, '<C-s>', 'belowright horizontal')
+          map_split(buf_id, '<C-v>', 'belowright vertical')
+        end,
+      })
+    end,
     keys = {
       {
         '<leader>fm',
