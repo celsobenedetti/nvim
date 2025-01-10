a9e005141403fe655c9784e25c3b86e7a224280b
Celso Benedetti <celsobenedetti2@gmail.com>
2025-01-09 21:00:57 -0300

    feat: dropbar.nvim

---
+++ lua/modules/ui/dropbar.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    'Bekaboo/dropbar.nvim',
+    lazy = C.opt.performance,
+    -- dependencies = { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    config = function()
+      local config = require 'dropbar.configs'
+      config.opts.icons.kinds.dir_icon = function()
+        return '', 'Comment'
+      end
+
+      local dropbar_api = require 'dropbar.api'
+      vim.keymap.set('n', '<Leader>;', dropbar_api.pick, { desc = 'Pick symbols in winbar' })
+      -- vim.keymap.set('n', '[;', dropbar_api.goto_context_start, { desc = 'Go to start of current context' })
+      -- vim.keymap.set('n', '];', dropbar_api.select_next_context, { desc = 'Select next context' })
+    end,
+  },
+}
