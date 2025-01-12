9e466f53d5ef84d9929bbc88c505679aa0874751
Celso Benedetti <celsobenedetti2@gmail.com>
2025-01-12 18:52:32 -0300

    fix: go dap debugger

--- lua/modules/dap/dap.lua
+++ lua/modules/dap/dap.lua
@@ -114,7 +114,25 @@ return {
 
       if filetype == 'go' then
         -- Install golang specific config
-        require('dap-go').setup()
+        require('dap-go').setup {
+          dap_configurations = {
+            {
+              type = 'go',
+              name = 'Attach remote',
+              mode = 'remote',
+              request = 'attach',
+              -- tell which host and port to connect to
+              connect = {
+                host = '127.0.0.1',
+                port = '8181',
+              },
+            },
+          },
+          delve = {
+            port = '8181',
+          },
+        }
+
         return
       end
     end,
