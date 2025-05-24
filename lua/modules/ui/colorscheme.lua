2318f6537a17107cffd1070e76f6f8c2e0f722d1
Celso Benedetti <celsobenedetti2@gmail.com>
2025-05-24 19:05:27 -0300

    perf: all colorschemes are lazy

--- lua/modules/ui/colorscheme.lua
+++ lua/modules/ui/colorscheme.lua
@@ -1,18 +1,8 @@
 return {
-  -- { 'catppuccin/nvim' },
-  { 'ellisonleao/gruvbox.nvim', priority = 1000, config = true, lazy = true },
-  {
-    'rose-pine/neovim',
-    lazy = true,
-    name = 'rose-pine',
-    opts = {
-      styles = { italic = false },
-    },
-  },
-  {
-    'projekt0n/github-nvim-theme',
-    lazy = true, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    opts = true,
-  },
+  { lazy = true, 'catppuccin/nvim' },
+  { lazy = true, 'nyoom-engineering/oxocarbon.nvim' },
+  { lazy = true, 'rebelot/kanagawa.nvim' },
+  { lazy = true, 'ellisonleao/gruvbox.nvim', priority = 1000, config = true },
+  { lazy = true, 'projekt0n/github-nvim-theme', priority = 1000, opts = true },
+  { lazy = true, 'rose-pine/neovim', name = 'rose-pine', opts = { styles = { italic = false } } },
 }
