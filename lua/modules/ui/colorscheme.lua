0125386ecedfcd4884b2714a88c776698d47c5df
Celso Benedetti <celsobenedetti2@gmail.com>
2025-06-02 23:02:07 -0300

    add: evergarden colorscheme

--- lua/modules/ui/colorscheme.lua
+++ lua/modules/ui/colorscheme.lua
@@ -13,4 +13,26 @@ return {
   { lazy = true, 'olivercederborg/poimandres.nvim', priority = 1000, opts = true },
   { lazy = true, 'drewxs/ash.nvim', priority = 1000 },
   { lazy = true, 'slugbyte/lackluster.nvim', priority = 1000 },
+  {
+    'everviolet/nvim',
+    name = 'evergarden',
+    priority = 1000, -- Colorscheme plugin is loaded first before any other plugins
+    opts = {
+      theme = {
+        variant = 'fall', -- 'winter'|'fall'|'spring'|'summer'
+        accent = 'green',
+      },
+      editor = {
+        transparent_background = false,
+        sign = { color = 'none' },
+        float = {
+          color = 'mantle',
+          invert_border = false,
+        },
+        completion = {
+          color = 'surface0',
+        },
+      },
+    },
+  },
 }
