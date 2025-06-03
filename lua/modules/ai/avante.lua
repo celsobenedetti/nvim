515463244ed22916309f12d56e9c797a78f79981
Celso Benedetti <celsobenedetti2@gmail.com>
2025-06-03 15:56:15 -0300

    cleanup: avante.nvim keymaps code

--- lua/modules/ai/avante.lua
+++ lua/modules/ai/avante.lua
@@ -1,51 +1,7 @@
 return {
   {
     'yetone/avante.nvim',
-    -- event = 'VeryLazy',
-    lazy = true,
-    keys = function(_, keys)
-      local opts = require('lazy.core.plugin').values(require('lazy.core.config').spec.plugins['avante.nvim'], 'opts', false)
-
-      local mappings = {
-        {
-          opts.mappings.ask,
-          function()
-            require('avante.api').ask()
-          end,
-          desc = 'avante: ask',
-          mode = { 'n', 'v' },
-        },
-        {
-          opts.mappings.refresh,
-          function()
-            require('avante.api').refresh()
-          end,
-          desc = 'avante: refresh',
-          mode = 'v',
-        },
-        {
-          opts.mappings.edit,
-          function()
-            require('avante.api').edit()
-          end,
-          desc = 'avante: edit',
-          mode = { 'n', 'v' },
-        },
-
-        {
-          opts.mappings.clear,
-          function()
-            vim.cmd 'AvanteClear'
-          end,
-          desc = 'avante: clear',
-          mode = { 'n', 'v' },
-        },
-      }
-      mappings = vim.tbl_filter(function(m)
-        return m[1] and #m[1] > 0
-      end, mappings)
-      return vim.list_extend(mappings, keys)
-    end,
+    event = 'VeryLazy',
     version = false, -- set this if you want to always pull the latest change
     opts = {
       provider = 'openai',
