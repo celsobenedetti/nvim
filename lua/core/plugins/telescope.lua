14ad96c633cda22d13bd44686e5506d8a79b8b7c
Celso Benedetti <celsobenedetti2@gmail.com>
2025-06-04 11:31:48 -0300

    telescope: add register mapping

--- lua/core/plugins/telescope.lua
+++ lua/core/plugins/telescope.lua
@@ -124,6 +124,7 @@ return {
       map('n', '<leader>st', builtin.builtin, { desc = 'Telescope [S]earch [S]elect Telescope' })
       map('n', '<leader>tm', builtin.marks, { desc = 'Telescope Marks' })
       map('n', '<leader>tr', builtin.oldfiles, { desc = 'Telescope Recent (oldfiles)' })
+      map('n', '<leader>t"', builtin.registers, { desc = 'Telescope Registries' })
       map('n', '<leader>sw', builtin.grep_string, { desc = 'Telescope [S]earch current [W]ord' })
       map('v', '<leader>sw', function()
         local selection = C.utils.get_visual_selection()
