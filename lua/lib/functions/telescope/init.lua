be655ae1d9ba59854bb84b6b289f2b49cf7546e5
Celso Benedetti <celsobenedetti2@gmail.com>
2025-06-05 00:40:43 -0300

    feat(telescope): find org_files

--- lua/lib/functions/telescope/init.lua
+++ lua/lib/functions/telescope/init.lua
@@ -4,5 +4,6 @@ M.dotfiles = require('lib.functions.telescope.dotfiles').run
 M.notes = require('lib.functions.telescope.notes').run
 M.move_file = require('lib.functions.telescope.move_file').run
 M.vertical_tabs = require('lib.functions.telescope.vertical_tabs').run
+M.org_files = require('lib.functions.telescope.org_files').run
 
 return M
