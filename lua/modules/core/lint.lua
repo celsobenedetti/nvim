a8e0f0b72d2def1107a6f8c3c0492fa65e83c174
Celso Benedetti <celsobenedetti2@gmail.com>
2025-07-21 09:57:09 -0300

    core: lint

---
+++ lua/modules/core/lint.lua
@@ -0,0 +1,42 @@
+return {
+  {
+    "mfussenegger/nvim-lint",
+    event = "VeryLazy",
+    opts = function(_, opts)
+      opts.linters_by_ft = vim.tbl_deep_extend("keep", opts.linters_by_ft or {}, {
+        bash = { "shellcheck", "bash" },
+        sh = { "shellcheck" },
+        make = { "checkmake" },
+        -- terraform = { "trivy" },
+        -- typescript = { "oxlint" },
+        -- javascript = { "oxlint" },
+        -- zsh = { "zsh" },
+      })
+
+      vim.api.nvim_create_user_command("LintInfo", function()
+        local filetype = vim.bo.filetype
+        local linters = require("lint").linters_by_ft[filetype]
+
+        if linters then
+          print("Linters for " .. filetype .. ": " .. table.concat(linters, ", "))
+        else
+          print("No linters configured for filetype: " .. filetype)
+        end
+      end, {})
+    end,
+  },
+
+  {
+
+    "mason-org/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "shellcheck",
+        "shfmt",
+        "checkmake",
+        "trivy",
+        "oxlint",
+      },
+    },
+  },
+}
