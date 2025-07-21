7d0c9553c9e7f7d6649ce3debf59d2c30cef0c59
Celso Benedetti <celsobenedetti2@gmail.com>
2025-07-21 00:17:14 -0300

    git: gitsigns mappings

---
+++ lua/modules/git/gitsigns.lua
@@ -0,0 +1,43 @@
+return {
+  {
+    "lewis6991/gitsigns.nvim",
+    keys = {
+      {
+        "<leader>gA",
+        function()
+          local gs = package.loaded.gitsigns
+          gs.stage_buffer()
+        end,
+        desc = "gitsigns: stage buffer",
+      },
+      {
+        "<leader>gR",
+        function()
+          local gs = package.loaded.gitsigns
+          gs.reset_buffer()
+        end,
+        desc = "gitsigns: reset buffer",
+      },
+
+      {
+        "<leader>ga",
+        function()
+          local gs = package.loaded.gitsigns
+          gs.stage_hunk()
+        end,
+        desc = "gitsigns: stage hunk",
+      },
+
+      {
+        "<leader>gs",
+        function()
+          local gs = package.loaded.gitsigns
+          gs.reset_hunk()
+        end,
+        desc = "gitsigns: reset hunk",
+      },
+      { "[g", ":Gitsigns prev_hunk<CR>", desc = "Prev git diff hunk" },
+      { "]g", ":Gitsigns next_hunk<CR>", desc = "Next git diff hunk" },
+    },
+  },
+}
