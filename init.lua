48d5b8701e59a6eaf11ac42c04279d53e1c033c5
Celso Benedetti <celsobenedetti2@gmail.com>
2025-07-21 11:08:18 -0300

    rm: mini.snippets

--- init.lua
+++ init.lua
@@ -24,7 +24,7 @@ require("lazy").setup({
 
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
-    { import = "lazyvim.plugins.extras.coding.mini-snippets" },
+    { import = "lazyvim.plugins.extras.coding.blink" },
     { import = "lazyvim.plugins.extras.coding.neogen" },
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.editor.mini-files" },
