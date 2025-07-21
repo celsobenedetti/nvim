935bf1de32d4a35f49d6e4eb8db4257e6bfbea92
Celso Benedetti <celsobenedetti2@gmail.com>
2025-07-21 00:17:27 -0300

    globals: zk

---
+++ lua/lib/zk.lua
@@ -0,0 +1,14 @@
+local M = {}
+
+M.open_orgmode_or_obsidian_link = function()
+  local status, orgmode = pcall(require, "orgmode")
+  if status then
+    orgmode.action("org_mappings.open_at_point")
+  end
+  vim.cmd("ObsidianFollowLink")
+
+  -- gf go to file
+  vim.api.nvim_feedkeys(Keys("gf"), "n", true)
+end
+
+return M
