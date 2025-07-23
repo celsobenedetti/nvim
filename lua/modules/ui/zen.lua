b112c6b504a6fcd2ebed1143033a1e776be9b281
Celso Benedetti <celsobenedetti2@gmail.com>
2025-07-22 23:25:25 -0300

    config: zen

---
+++ lua/modules/ui/zen.lua
@@ -0,0 +1,24 @@
+return {
+
+  "folke/snacks.nvim",
+  opts = function(_, opts)
+    Snacks.toggle.dim():map("<leader>tw")
+
+    Snacks.config.style("zen", {
+      enter = true,
+      fixbuf = false,
+      minimal = false,
+      width = 120,
+      height = 0,
+      backdrop = { transparent = false, blend = 40 },
+      keys = { q = false },
+      zindex = 40,
+      wo = {
+        winhighlight = "NormalFloat:Normal",
+      },
+      w = {
+        snacks_main = true,
+      },
+    })
+  end,
+}
