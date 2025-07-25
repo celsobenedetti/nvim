57ab4210631ca7a16307b144d9478b6a0ba9ead4
Celso Benedetti <celsobenedetti2@gmail.com>
2025-07-24 22:29:45 -0300

    obsidian: use snacks for picker

--- lua/modules/zk/obsidian.lua
+++ lua/modules/zk/obsidian.lua
@@ -79,6 +79,25 @@ return {
         return title
       end
 
+      opts.picker = {
+        -- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', 'mini.pick' or 'snacks.pick'.
+        name = "snacks.pick",
+        -- Optional, configure key mappings for the picker. These are the defaults.
+        -- Not all pickers support all mappings.
+        note_mappings = {
+          -- Create a new note from your query.
+          new = "<C-x>",
+          -- Insert a link to the selected note.
+          insert_link = "<C-l>",
+        },
+        tag_mappings = {
+          -- Add tag(s) to current note.
+          tag_note = "<C-x>",
+          -- Insert a tag at the current location.
+          insert_tag = "<C-l>",
+        },
+      }
+
       -- TODO: costumize frontmatter fucntion so we don't care about frontmatter for certain files
       opts.note_frontmatter_func = function(note)
         local frontmatter = {
