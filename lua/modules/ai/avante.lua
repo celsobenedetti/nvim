e65eceb0a06e4c16212788bea1f1ebe85851e4da
Celso Benedetti <celsobenedetti2@gmail.com>
2025-07-27 20:34:38 -0300

    avante: fix config

--- lua/modules/ai/avante.lua
+++ lua/modules/ai/avante.lua
@@ -1,37 +1,25 @@
-return {
+-- recommended: views can only be fully collapsed with the global statusline
+vim.opt.laststatus = 3
 
+return {
   {
     "yetone/avante.nvim",
     event = "VeryLazy",
     version = false, -- set this if you want to always pull the latest change
-    opts = function(_, opts)
-      opts = vim.tbl_deep_extend("force", opts or {}, {
-        provider = "openai",
-        model = "chatgpt-4o-latest",
-        -- provider = 'claude',
-        -- mode = 'agentic',
-        -- claude = {
-        --   endpoint = 'https://api.anthropic.com',
-        --   -- model = 'claude-3-5-sonnet-20241022',
-        --   model = 'claude-sonnet-4-20250514',
-        --   temperature = 0,
-        --   max_tokens = 4096,
-        -- },
-        mappings = {
-          ask = "<leader>aa", -- ask
-          edit = "<leader>ae", -- edit
-          refresh = "<leader>ar", -- refresh
-          clear = "<leader>aC", -- clear
-        },
+    opts = {
+      provider = "openai",
+      model = "chatgpt-4o-latest",
+      mappings = {
+        ask = "<leader>aa", -- ask
+        edit = "<leader>ae", -- edit
+        refresh = "<leader>ar", -- refresh
+        clear = "<leader>aC", -- clear
+      },
 
-        override_prompt_dir = function()
-          return vim.fn.expand("~/.config/ai-rules/avante")
-        end,
-      })
-
-      -- recommended: views can only be fully collapsed with the global statusline
-      vim.opt.laststatus = 3
-    end,
+      override_prompt_dir = function()
+        return vim.fn.expand("~/.config/ai-rules/avante")
+      end,
+    },
 
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
     build = "make",
