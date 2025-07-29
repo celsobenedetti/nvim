b362402091cd13ba12db774467da38d1fc9796b7
Celso Benedetti <celsobenedetti2@gmail.com>
2025-07-28 22:06:04 -0300

    fix(lib.term):pcall for nvim_open_win
      I'm introducing some retry-recursion here.
      this may well introduce an infinite loop.
      Here's hoping it doesn't :)

--- lua/lib/window.lua
+++ lua/lib/window.lua
@@ -18,6 +18,12 @@ M.below = function(buffer)
   return win
 end
 
+-- spawn a window on the bottom of the screen with a :term buffer
+-- this is a "float" window, which unfortunately does not behave like a splits.
+-- to navigate to the float window, we need some workaround like nvim_set_current_win
+--
+-- really the only reason I use this is vibes based, because the "border" decoration looks so nice
+--
 ---@param buffer? number the buffer to attach to window
 ---@return number - the window id
 M.relative_below = function(buffer, opts)
@@ -25,8 +31,8 @@ M.relative_below = function(buffer, opts)
   local api = vim.api
   local columns = vim.api.nvim_get_option_value("columns", {})
   local lines = vim.api.nvim_get_option_value("lines", {})
-  local win_height = math.max(8, math.floor(lines * 0.30))
-  local win = api.nvim_open_win(buf, true, {
+  local win_height = math.max(8, math.floor(lines * 0.34))
+  local win_opts = {
     relative = "editor",
     row = lines - win_height,
     col = 0,
@@ -34,7 +40,16 @@ M.relative_below = function(buffer, opts)
     height = win_height,
     style = "minimal",
     border = "single",
-  })
+  }
+
+  local ok, win = pcall(api.nvim_open_win, buf, true, win_opts)
+  if not ok then
+    -- WARN: this sure looks like an infinite loop vector.
+    -- I'm trusting nvim_open_win will always have a valid buffer from nvim_create_buf
+    buf = api.nvim_create_buf(false, true)
+    return M.relative_below(buf, opts)
+  end
+
   return win
 end
 
