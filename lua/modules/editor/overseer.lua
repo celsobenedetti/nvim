bacbe5ef657e791abe835a8d89ad6bdcac7e3a79
Celso Benedetti <celsobenedetti2@gmail.com>
2025-08-04 21:58:21 -0300

    add: overseer.nvim

---
+++ lua/modules/editor/overseer.lua
@@ -0,0 +1,96 @@
+return {
+  {
+    'stevearc/overseer.nvim',
+    cmd = {
+      'OverseerOpen',
+      'OverseerClose',
+      'OverseerToggle',
+      'OverseerSaveBundle',
+      'OverseerLoadBundle',
+      'OverseerDeleteBundle',
+      'OverseerRunCmd',
+      'OverseerRun',
+      'OverseerInfo',
+      'OverseerBuild',
+      'OverseerQuickAction',
+      'OverseerTaskAction',
+      'OverseerClearCache',
+    },
+    opts = {
+      dap = false,
+      task_list = {
+        bindings = {
+          ['<C-h>'] = false,
+          ['<C-j>'] = false,
+          ['<C-k>'] = false,
+          ['<C-l>'] = false,
+        },
+      },
+      form = {
+        win_opts = {
+          winblend = 0,
+        },
+      },
+      confirm = {
+        win_opts = {
+          winblend = 0,
+        },
+      },
+      task_win = {
+        win_opts = {
+          winblend = 0,
+        },
+      },
+    },
+    -- stylua: ignore
+    keys = {
+      { "<leader>OR", "<cmd>OverseerRun<cr>",         desc = "Overseer: Run task" },
+      { "<leader>run", "<cmd>OverseerRun<cr>",         desc = "Overseer: Run task" },
+      { "<leader>OT", "<cmd>OverseerToggle<cr>",      desc = "Overseer: Toggle" },
+      { "<leader>OQ", "<cmd>OverseerQuickAction<cr>", desc = "Overseer: Action recent task" },
+      { "<leader>OI", "<cmd>OverseerInfo<cr>",        desc = "Overseer: Overseer Info" },
+      { "<leader>OB", "<cmd>OverseerBuild<cr>",       desc = "Overseer: Task builder" },
+      { "<leader>OA", "<cmd>OverseerTaskAction<cr>",  desc = "Overseer: Task action" },
+      { "<leader>OC", "<cmd>OverseerClearCache<cr>",  desc = "Overseer: Clear cache" },
+    },
+  },
+  {
+    'folke/which-key.nvim',
+    optional = true,
+    opts = {
+      spec = {
+        { '<leader>o', group = 'overseer' },
+      },
+    },
+  },
+  {
+    'folke/edgy.nvim',
+    optional = true,
+    opts = function(_, opts)
+      opts.right = opts.right or {}
+      table.insert(opts.right, {
+        title = 'Overseer',
+        ft = 'OverseerList',
+        open = function()
+          require('overseer').open()
+        end,
+      })
+    end,
+  },
+  {
+    'nvim-neotest/neotest',
+    optional = true,
+    opts = function(_, opts)
+      opts = opts or {}
+      opts.consumers = opts.consumers or {}
+      opts.consumers.overseer = require 'neotest.consumers.overseer'
+    end,
+  },
+  {
+    'mfussenegger/nvim-dap',
+    optional = true,
+    opts = function()
+      require('overseer').enable_dap()
+    end,
+  },
+}
