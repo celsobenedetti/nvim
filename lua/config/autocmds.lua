1c9d12acd2da12d13b5cf6db8c90067510feb7b3
Celso Benedetti <celsobenedetti2@gmail.com>
2025-08-04 21:57:39 -0300

    autocmds: always insert mode when entering terminal

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,6 +1,3 @@
-function Augroup(name)
-  return vim.api.nvim_create_augroup("celso_augruop_" .. name, { clear = true })
-end
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 --
@@ -9,70 +6,78 @@ end
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
---
+
+function Augroup(name)
+  return vim.api.nvim_create_augroup('celso_augroup_' .. name, { clear = true })
+end
 
 -- insert mode when entering git commit
-vim.api.nvim_create_autocmd("VimEnter", {
-  group = Augroup("Git_Commit_Editor"),
-  pattern = { "COMMIT_EDITMSG", "new_note" },
+vim.api.nvim_create_autocmd('VimEnter', {
+  desc = 'Insert mode when entering git commit',
+  group = Augroup 'Git_Commit_Editor',
+  pattern = { 'COMMIT_EDITMSG', 'new_note' },
   callback = function()
-    vim.api.nvim_feedkeys(Keys("i<BS>"), "n", true)
+    vim.api.nvim_feedkeys(Keys 'i<BS>', 'n', true)
   end,
 })
 
-vim.api.nvim_create_autocmd({ "VimEnter" }, {
+vim.api.nvim_create_autocmd({ 'VimEnter' }, {
+  desc = 'Run on all files',
   callback = function()
-    vim.cmd("set formatoptions-=cro") -- Stop comment continuation on line below
+    vim.cmd 'set formatoptions-=cro' -- Stop comment continuation on line below
   end,
-  group = Augroup("Run_on_VimEnter"),
-  desc = "Run on all files",
+  group = Augroup 'Run_on_VimEnter',
 })
 
 -- VimEnter autocmd should fold all
 -- BufRead autocmd should fold frontmatter
-local markdown_group = Augroup("Markdown")
-local markdown = "*.md"
-vim.api.nvim_create_autocmd("VimEnter", {
+local markdown_group = Augroup 'Markdown'
+local markdown = '*.md'
+vim.api.nvim_create_autocmd('VimEnter', {
+  desc = 'Fold all headings when entering markdown files',
   pattern = markdown,
   group = markdown_group,
   callback = function()
     -- nested markdown folding
-    vim.cmd("set foldexpr=NestedMarkdownFolds()")
+    vim.cmd 'set foldexpr=NestedMarkdownFolds()'
 
     vim.defer_fn(function()
       -- fold all
-      vim.api.nvim_feedkeys(Keys("zM"), "n", true)
+      vim.api.nvim_feedkeys(Keys 'zM', 'n', true)
 
       vim.defer_fn(function()
         -- go do #heading1
-        vim.api.nvim_feedkeys(Keys("G"), "n", true)
+        vim.api.nvim_feedkeys(Keys 'G', 'n', true)
 
         vim.defer_fn(function()
           -- unfold it
-          vim.api.nvim_feedkeys(Keys("zo"), "n", true)
+          vim.api.nvim_feedkeys(Keys 'zo', 'n', true)
         end, 60)
       end, 60)
     end, 100)
   end,
 })
 
-vim.api.nvim_create_autocmd("BufEnter", {
+vim.api.nvim_create_autocmd('BufEnter', {
+  desc = 'Fold frontmatter when entering markdown file',
   pattern = markdown,
   group = markdown_group,
   callback = function()
     vim.defer_fn(function()
-      require("lib.markdown").fold_frontmatter()
+      require('lib.markdown').fold_frontmatter()
     end, 100)
   end,
 })
 
-vim.api.nvim_create_autocmd("TermOpen", {
+vim.api.nvim_create_autocmd({ 'TermOpen', 'BufWinEnter', 'WinEnter' }, {
+  desc = 'Always insert mode when entering a terminal',
+  pattern = 'term://*',
   callback = function()
-    vim.api.nvim_feedkeys("i", "n", true)
+    vim.cmd 'startinsert'
   end,
-  group = vim.api.nvim_create_augroup("custom-term-open", { clear = true }),
+  group = Augroup 'term-enter',
 })
 
-if os.getenv("IS_ZEN") == "true" then
-  require("lib.startup.zen")
+if os.getenv 'IS_ZEN' == 'true' then
+  require 'lib.startup.zen'
 end
