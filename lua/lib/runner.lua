5f0c396e857ba05819d8d06295e0c35cea19cb6a
Celso Benedetti <celsobenedetti2@gmail.com>
2025-08-07 20:33:48 -0300

    note: lib.runner is sticking around

--- lua/lib/runner.lua
+++ lua/lib/runner.lua
@@ -1,5 +1,5 @@
-local cwd = require("lib.cwd")
-local strings = require("lib.strings")
+local cwd = require 'lib.cwd'
+local strings = require 'lib.strings'
 
 local M = {}
 
@@ -20,49 +20,49 @@ local function run_targets(targets)
       cmd = target.cmd,
       desc = target.desc,
       preview = {
-        ft = "json",
+        ft = 'json',
         text = vim.inspect(target),
       },
     })
   end
 
-  Snacks.picker.pick({
+  Snacks.picker.pick {
     items = snack_items,
-    preview = "preview", -- Add this line
+    preview = 'preview', -- Add this line
     format = function(item, picker)
       return { { item.desc } }
     end,
     confirm = function(picker, item)
-      local selected = picker:selected({ fallback = true })
+      local selected = picker:selected { fallback = true }
 
       -- "confirmed" item will not be in the selected, unless manually selected
       local confirmedFound = false
 
       -- for item in selected
       for _, i in ipairs(selected) do
-        M.spawn_term({ title = i.file, cmd = i.cmd, desc = i.desc })
+        M.spawn_term { title = i.file, cmd = i.cmd, desc = i.desc }
 
         if i.cmd == item.cmd then
           confirmedFound = true
         end
       end
       if not confirmedFound then
-        M.spawn_term({ title = item.title, cmd = item.cmd, desc = item.desc })
+        M.spawn_term { title = item.title, cmd = item.cmd, desc = item.desc }
       end
       picker:close()
     end,
-  })
+  }
 end
 
 -- read package.json and return scripts section
 ---@return runner_target[] | nil
 local function read_package_json_scripts()
-  local package_json = cwd.cwd() .. "/package.json"
-  local file = io.open(package_json, "r")
+  local package_json = cwd.cwd() .. '/package.json'
+  local file = io.open(package_json, 'r')
   if not file then
     return nil
   end
-  local content = file:read("*all")
+  local content = file:read '*all'
   file:close()
   local contents = vim.json.decode(content)
 
@@ -70,7 +70,7 @@ local function read_package_json_scripts()
   for k, v in pairs(contents.scripts) do
     table.insert(scripts, {
       desc = k,
-      cmd = "pnpm " .. k,
+      cmd = 'pnpm ' .. k,
       title = v,
     })
   end
@@ -79,8 +79,8 @@ end
 
 --- @return runner_target[] | nil
 local function read_justfile_targets()
-  local output = vim.fn.system("just --list")
-  local err = vim.api.nvim_get_vvar("shell_error") ~= 0
+  local output = vim.fn.system 'just --list'
+  local err = vim.api.nvim_get_vvar 'shell_error' ~= 0
   if err then
     return nil
   end
@@ -89,7 +89,7 @@ local function read_justfile_targets()
   local targets = {}
 
   local skip = true
-  for line in output:gmatch("[^\r\n]+") do
+  for line in output:gmatch '[^\r\n]+' do
     -- skip first line
     if skip then
       skip = false
@@ -99,17 +99,17 @@ local function read_justfile_targets()
     line = strings.trim(line)
     line = strings.collapse_whitespace(line)
 
-    local parts = vim.fn.split(line, "#")
+    local parts = vim.fn.split(line, '#')
     local cmd = strings.trim(parts[1])
     local desc = cmd
     if #parts > 1 then
-      desc = cmd .. ":" .. parts[2]
+      desc = cmd .. ':' .. parts[2]
     end
 
     table.insert(targets, {
       desc = desc,
-      cmd = "just " .. cmd,
-      title = "just_" .. cmd,
+      cmd = 'just ' .. cmd,
+      title = 'just_' .. cmd,
     })
     ::continue::
   end
@@ -119,9 +119,9 @@ end
 --- spawns new tabterm with cmd
 ---@param term runner_target
 M.spawn_term = function(term)
-  vim.cmd("tabnew")
-  vim.cmd("terminal " .. term.cmd)
-  vim.api.nvim_buf_set_name(0, term.title .. ".zsh")
+  vim.cmd 'tabnew'
+  vim.cmd('terminal ' .. term.cmd)
+  vim.api.nvim_buf_set_name(0, term.title .. '.zsh')
 end
 
 -- 1. read targets from Justfile
