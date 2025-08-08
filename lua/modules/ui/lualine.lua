d616542ebcd5b9fa1acb5e36f0dea2f0547dc34f
Celso Benedetti <celsobenedetti2@gmail.com>
2025-08-07 21:00:23 -0300

    disable: lualine

--- lua/modules/ui/lualine.lua
+++ lua/modules/ui/lualine.lua
@@ -1,10 +1,12 @@
 return {
-  "nvim-lualine/lualine.nvim",
+  'nvim-lualine/lualine.nvim',
+  enabled = vim.g.lualine,
+  cmd = 'Lualine',
   opts = function(_, opts)
-    opts.opts = vim.tbl_deep_extend("force", opts.opts or {}, {
+    opts.opts = vim.tbl_deep_extend('force', opts.opts or {}, {
 
       theme = {
-        terminal = { fg = "#ffffff", bg = "#000000" }, -- Example: White text on black background
+        terminal = { fg = '#ffffff', bg = '#000000' }, -- Example: White text on black background
       },
     })
   end,
