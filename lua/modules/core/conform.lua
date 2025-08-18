38ae8dd113dd7f6a1582fe43efac0853e7464b9d
Celso Benedetti <celsobenedetti2@gmail.com>
2025-08-18 20:24:23 -0300

    configm: gitcommit

--- lua/modules/core/conform.lua
+++ lua/modules/core/conform.lua
@@ -8,6 +8,7 @@ return {
   opts = function(_, opts)
     opts.formatters_by_ft = vim.tbl_deep_extend('force', opts.formatters_by_ft or {}, {
       markdown = { 'mfmt' },
+      gitcommit = { 'mfmt' },
       org = { 'mfmt_org' },
       javascript = { 'prettierd' },
       typescript = { 'prettierd' },
