7054741fa146c7f6f190ea5cfc7da34a9f8e4ece
Celso Benedetti <celsobenedetti2@gmail.com>
2025-09-02 20:25:21 -0300

    remove: undotree

--- lua/modules/editor/undotree.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'mbbill/undotree',
-  lazy = false,
-  keys = { { '<leader>U', '<cmd>UndotreeToggle<cr>', desc = 'Undotree Toggle' } },
-  config = function()
-    vim.opt.undodir = os.getenv 'HOME' .. '/.vim/undodir'
-  end,
-}
