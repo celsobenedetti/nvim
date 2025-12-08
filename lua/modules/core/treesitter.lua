d2ba1b0f590e995dd0498a70856ef2f8292abe9a
Celso Benedetti <celsobenedetti2@gmail.com>
2025-12-08 17:16:41 -0300

    feat: flash + treesitter

--- lua/modules/core/treesitter.lua
+++ lua/modules/core/treesitter.lua
@@ -1,31 +1,185 @@
+-- return {
+--   'nvim-treesitter/nvim-treesitter',
+--   opts = {
+--   },
+-- }
+--
 return {
-  'nvim-treesitter/nvim-treesitter',
-  opts = {
-    ensure_installed = {
-      'bash',
-      'c',
-      'diff',
-      'html',
-      'javascript',
-      'jsdoc',
-      'json',
-      -- 'jsonc',
-      'lua',
-      'luadoc',
-      'luap',
-      'markdown',
-      'markdown_inline',
-      'printf',
-      'python',
-      'query',
-      'regex',
-      'toml',
-      'tsx',
-      'typescript',
-      'vim',
-      'vimdoc',
-      'xml',
-      'yaml',
+
+  -- Treesitter is a new parser generator tool that we can
+  -- use in Neovim to power faster and more accurate
+  -- syntax highlighting.
+  {
+    'nvim-treesitter/nvim-treesitter',
+    branch = 'main',
+    version = false, -- last release is way too old and doesn't work on Windows
+    build = function()
+      local TS = require 'nvim-treesitter'
+      if not TS.get_installed then
+        Snacks.notify.error 'Please restart Neovim and run `:TSUpdate` to use the `nvim-treesitter` **main** branch.'
+        return
+      end
+      TS.update(nil, { summary = true })
+    end,
+    event = { 'VeryLazy' },
+    cmd = { 'TSUpdate', 'TSInstall', 'TSLog', 'TSUninstall' },
+    opts_extend = { 'ensure_installed' },
+    ---@alias lazyvim.TSFeat { enable?: boolean, disable?: string[] }
+    ---@class lazyvim.TSConfig: TSConfig
+    opts = {
+      indent = { enable = true }, ---@type lazyvim.TSFeat
+      highlight = { enable = true }, ---@type lazyvim.TSFeat
+      folds = { enable = true }, ---@type lazyvim.TSFeat
+      ensure_installed = {
+        'bash',
+        'c',
+        'diff',
+        'html',
+        'javascript',
+        'jsdoc',
+        'json',
+        'jsonc',
+        'lua',
+        'luadoc',
+        'luap',
+        'markdown',
+        'markdown_inline',
+        'printf',
+        'python',
+        'query',
+        'regex',
+        'toml',
+        'tsx',
+        'typescript',
+        'vim',
+        'vimdoc',
+        'xml',
+        'yaml',
+      },
     },
+    ---@param opts lazyvim.TSConfig
+    config = function(_, opts)
+      local TS = require 'nvim-treesitter'
+
+      setmetatable(require 'nvim-treesitter.install', {
+        __newindex = function(_, k)
+          if k == 'compilers' then
+            vim.schedule(function()
+              Snacks.notify.error {
+                'Setting custom compilers for `nvim-treesitter` is no longer supported.',
+                '',
+                'For more info, see:',
+                '- [compilers](https://docs.rs/cc/latest/cc/#compile-time-requirements)',
+              }
+            end)
+          end
+        end,
+      })
+
+      -- some quick sanity checks
+      if not TS.get_installed then
+        return Snacks.notify.error 'Please use `:Lazy` and update `nvim-treesitter`'
+      elseif type(opts.ensure_installed) ~= 'table' then
+        return Snacks.notify.error '`nvim-treesitter` opts.ensure_installed must be a table'
+      end
+
+      -- setup treesitter
+      TS.setup(opts)
+      vim.wo.foldmethod = 'expr'
+      vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+    end,
+  },
+
+  -- {
+  --   'nvim-treesitter/nvim-treesitter-textobjects',
+  --   branch = 'main',
+  --   event = 'VeryLazy',
+  --   opts = {
+  --     move = {
+  --       enable = true,
+  --       set_jumps = true, -- whether to set jumps in the jumplist
+  --       keys = {
+  --         goto_next_start = { [']f'] = '@function.outer', [']c'] = '@class.outer', [']a'] = '@parameter.inner' },
+  --         goto_next_end = { [']F'] = '@function.outer', [']C'] = '@class.outer', [']A'] = '@parameter.inner' },
+  --         goto_previous_start = { ['[f'] = '@function.outer', ['[c'] = '@class.outer', ['[a'] = '@parameter.inner' },
+  --         goto_previous_end = { ['[F'] = '@function.outer', ['[C'] = '@class.outer', ['[A'] = '@parameter.inner' },
+  --       },
+  --     },
+  --   },
+  --   config = function(_, opts)
+  --     local TS = require 'nvim-treesitter-textobjects'
+  --     if not TS.setup then
+  --       Snacks.notify.error 'Please use `:Lazy` and update `nvim-treesitter`'
+  --       return
+  --     end
+  --
+  --     local function attach(buf)
+  --       local ft = vim.bo[buf].filetype
+  --       if not (vim.tbl_get(opts, 'move', 'enable')) then
+  --         return
+  --       end
+  --       ---@type table<string, table<string, string>>
+  --       local moves = vim.tbl_get(opts, 'move', 'keys') or {}
+  --
+  --       for method, keymaps in pairs(moves) do
+  --         for key, query in pairs(keymaps) do
+  --           local queries = type(query) == 'table' and query or { query }
+  --           local parts = {}
+  --           for _, q in ipairs(queries) do
+  --             local part = q:gsub('@', ''):gsub('%..*', '')
+  --             part = part:sub(1, 1):upper() .. part:sub(2)
+  --             table.insert(parts, part)
+  --           end
+  --           local desc = table.concat(parts, ' or ')
+  --           desc = (key:sub(1, 1) == '[' and 'Prev ' or 'Next ') .. desc
+  --           desc = desc .. (key:sub(2, 2) == key:sub(2, 2):upper() and ' End' or ' Start')
+  --           if not (vim.wo.diff and key:find '[cC]') then
+  --             vim.keymap.set({ 'n', 'x', 'o' }, key, function()
+  --               require('nvim-treesitter-textobjects.move')[method](query, 'textobjects')
+  --             end, {
+  --               buffer = buf,
+  --               desc = desc,
+  --               silent = true,
+  --             })
+  --           end
+  --         end
+  --       end
+  --     end
+  --
+  --     vim.api.nvim_create_autocmd('FileType', {
+  --       group = vim.api.nvim_create_augroup('celso_treesitter_textobjects', { clear = true }),
+  --       callback = function(ev)
+  --         attach(ev.buf)
+  --       end,
+  --     })
+  --     vim.tbl_map(attach, vim.api.nvim_list_bufs())
+  --   end,
+  -- },
+
+  -- Automatically add closing tags for HTML and JSX
+  {
+    'windwp/nvim-ts-autotag',
+    event = 'VeryLazy',
+    opts = {},
+  },
+
+  {
+    'nvim-treesitter/nvim-treesitter-context',
+    event = 'VeryLazy',
+    opts = function()
+      local tsc = require 'treesitter-context'
+      Snacks.toggle({
+        name = 'Treesitter Context',
+        get = tsc.enabled,
+        set = function(state)
+          if state then
+            tsc.enable()
+          else
+            tsc.disable()
+          end
+        end,
+      }):map '<leader>ut'
+      return { mode = 'cursor', max_lines = 3 }
+    end,
   },
 }
