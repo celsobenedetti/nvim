425595a3269b405ab023770a6084ee42b397aadf
Celso Benedetti <celsobenedetti2@gmail.com>
2025-12-08 23:01:01 -0300

    autcocmd: insert in git commit

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,7 +1,18 @@
+local autocmd = vim.api.nvim_create_autocmd
 local function augroup(name)
   return vim.api.nvim_create_augroup('celso_' .. name, { clear = true })
 end
---
+
+-- insert mode when entering git commit
+autocmd('VimEnter', {
+  desc = 'Insert mode when entering git commit',
+  group = augroup 'Git_Commit_Editor',
+  pattern = { 'COMMIT_EDITMSG', 'new_note' },
+  callback = function()
+    vim.api.nvim_feedkeys(Keys 'i<BS>', 'n', true)
+  end,
+})
+
 -- Highlight on yank
 vim.api.nvim_create_autocmd('TextYankPost', {
   group = augroup 'highlight_yank',
