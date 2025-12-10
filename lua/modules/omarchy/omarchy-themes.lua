a9f8f4292961c64eaaf463217e78087dd451ba88
Celso Benedetti <celsobenedetti2@gmail.com>
2025-12-10 17:28:32 -0300

    docs: comments

--- lua/modules/omarchy/omarchy-themes.lua
+++ lua/modules/omarchy/omarchy-themes.lua
@@ -1,7 +1,14 @@
 local omarchy_colorscheme = require('lib.colors').omarchy_colorscheme()
 
--- this is here simply to prevent these plugins from being cleaned up by lazy.nvim
 return {
+  -- add plugin from omarchy current theme and set colorscheme
+  vim.tbl_extend('keep', omarchy_colorscheme.colorscheme_plugin, {
+    init = function()
+      vim.cmd('colorscheme ' .. omarchy_colorscheme.colorscheme)
+    end,
+  }),
+
+  -- these are here simply to prevent these plugins from being cleaned up by lazy.nvim
   { 'tahayvr/matteblack.nvim', lazy = true, enabled = true },
   { 'EdenEast/nightfox.nvim', lazy = true, enabled = true },
   { 'gthelding/monokai-pro.nvim', lazy = true, enabled = true },
@@ -12,10 +19,4 @@ return {
   { 'ellisonleao/gruvbox.nvim', lazy = true, enabled = true },
   { 'kepano/flexoki-neovim', lazy = true, enabled = true },
   { 'everviolet/nvim', lazy = true, enabled = true, name = 'evergarden' },
-
-  vim.tbl_extend('keep', omarchy_colorscheme.colorscheme_plugin, {
-    init = function()
-      vim.cmd('colorscheme ' .. omarchy_colorscheme.colorscheme)
-    end,
-  }),
 }
