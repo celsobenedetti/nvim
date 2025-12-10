710969aea932ec13f15ae056860c72c03fa3f286
Celso Benedetti <celsobenedetti2@gmail.com>
2025-12-09 22:59:31 -0300

    feat: incline with icons

--- lua/modules/editor/mini.lua
+++ lua/modules/editor/mini.lua
@@ -4,5 +4,31 @@ return {
 
   -- ui
   { 'nvim-mini/mini.hipatterns', version = false, config = true },
-  { 'nvim-mini/mini.icons', version = false, config = true },
+  {
+    'nvim-mini/mini.icons',
+    version = false,
+    opts = {
+      file = {
+        ['.eslintrc.js'] = { glyph = '󰱺', hl = 'MiniIconsYellow' },
+        ['eslint.config.ts'] = { glyph = '󰱺', hl = 'MiniIconsYellow' },
+        ['.node-version'] = { glyph = '', hl = 'MiniIconsGreen' },
+        ['.nvmrc'] = { glyph = '', hl = 'MiniIconsGreen' },
+        ['.prettierrc'] = { glyph = '', hl = 'MiniIconsPurple' },
+        ['package.json'] = { glyph = '', hl = 'MiniIconsGreen' },
+        ['tsconfig.json'] = { glyph = '', hl = 'MiniIconsAzure' },
+        ['tsconfig.node.json'] = { glyph = '', hl = 'MiniIconsAzure' },
+        ['tsconfig.app.json'] = { glyph = '', hl = 'MiniIconsAzure' },
+        ['vite.config.ts'] = { glyph = '', hl = 'MiniIconsGreen' },
+        ['vitest.config.ts'] = { glyph = '', hl = 'MiniIconsGreen' },
+        ['pnpm-lock.yaml'] = { glyph = '', hl = 'MiniIconsYellow' },
+        ['pnpm-workspace.yaml'] = { glyph = '', hl = 'MiniIconsYellow' },
+      },
+    },
+    init = function()
+      package.preload['nvim-web-devicons'] = function()
+        require('mini.icons').mock_nvim_web_devicons()
+        return package.loaded['nvim-web-devicons']
+      end
+    end,
+  },
 }
