2c872b5a814977ba5ce25308e2a3f24ffa2d333d
Celso Benedetti <celsobenedetti2@gmail.com>
2025-12-11 09:53:47 -0300

    fix: overseer init setup

--- lua/config/overseer/overseer_init.lua
+++ lua/config/overseer/overseer_init.lua
@@ -1,30 +1,16 @@
-vim.g.mapleader = ' '
-vim.g.performance = true
-vim.o.background = 'dark'
-require('config.lazy')
-require('config.globals')
-
-local omarchy_colorscheme = require('lib.colors').omarchy_colorscheme()
-local colorscheme = omarchy_colorscheme.colorscheme
-local colorscheme_plugin = omarchy_colorscheme.colorscheme_plugin
-
-print(vim.inspect({ colorscheme_plugin }))
+require('config.init')
 
 require('lazy').setup({
   spec = {
-    colorscheme_plugin,
     { import = 'modules.overseer' },
     { import = 'modules.tmux' },
+    { import = 'modules.omarchy' },
     {
       'folke/snacks.nvim',
       opts = {
         picker = {},
       },
     },
-  },
-  checker = {
-    enabled = false, -- check for plugin updates periodically
-    notify = false, -- notify on update
   },
   performance = {
     rtp = {
@@ -41,8 +27,3 @@ require('lazy').setup({
     },
   },
 })
-
-vim.cmd.colorscheme(colorscheme)
-
-require('config.sensible')
-require('lib.modules.transparency')
