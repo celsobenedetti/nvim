9e1f73e72d6e283092bd2839f3513ec12873e253
Celso Benedetti <celsobenedetti2@gmail.com>
2025-12-15 13:37:59 -0300

    perf: use gitsigns instead of vim.fn.system for git branch

--- after/plugin/statusline.lua
+++ after/plugin/statusline.lua
@@ -1,13 +1,6 @@
-local cwd = require('lib.cwd')
 local has_icons, devicons = pcall(require, 'nvim-web-devicons')
 
 function _G.MyStatusLine()
-  local branch = ''
-
-  if cwd.is_git_repo() then
-    branch = '  ' .. vim.trim(vim.fn.system('git branch --show-current'))
-  end
-
   local relative_file = vim.fn.expand('%:.')
 
   local icon = ''
@@ -15,7 +8,8 @@ function _G.MyStatusLine()
     icon = devicons.get_icon(relative_file) or ''
   end
 
-  return branch .. '%=' .. icon .. ' ' .. relative_file .. '%=%='
+  local branch = vim.g.gitsigns_head or ''
+  return '  ' .. branch .. '%=' .. icon .. ' ' .. relative_file .. '%=%='
 end
 
 vim.opt.statusline = '%!v:lua.MyStatusLine()'
