e788919bb471751787e952401fe1700ff572b9d6
Celso Benedetti <celsobenedetti2@gmail.com>
2025-12-16 14:12:51 -0300

    perf(statusline): cache current_file

--- after/plugin/statusline.lua
+++ after/plugin/statusline.lua
@@ -13,7 +13,7 @@ local function _git_branch()
   return ' ' .. hl('Title', icons.git.branch) .. hl(HIGHLIGHT, (vim.g.gitsigns_head or ''))
 end
 
-local function _current_file()
+local function _file()
   local relative_file = vim.fn.expand('%:.')
   local icon = ''
   if has_icons then
@@ -51,7 +51,7 @@ local function _git_status()
   -- LSP clients attached to buffer
 end
 
-local function compute_lsps()
+local function _lsps()
   local bufnr = vim.api.nvim_get_current_buf()
   local clients = vim.lsp.get_clients({ bufnr = bufnr })
   if next(clients) == nil then
@@ -65,7 +65,7 @@ local function compute_lsps()
   return hl('Title', icons.lsp) .. hl(HIGHLIGHT, table.concat(vim.fn.reverse(c), ', '))
 end
 
-local function compute_formatters()
+local function _formatters()
   local ok, conform = pcall(require, 'conform')
   if not ok then
     return ''
@@ -84,6 +84,23 @@ local function compute_formatters()
 
   return hl('Title', ' ') .. hl(HIGHLIGHT, result)
 end
+
+-- Cache LSP clients on attach/detach
+vim.api.nvim_create_autocmd({ 'LspAttach', 'LspDetach' }, {
+  callback = function(args)
+    local bufnr = args.buf
+    vim.api.nvim_buf_set_var(bufnr, 'cached_lsps', _lsps())
+  end,
+})
+
+-- Cache formatters and file on buffer enter
+vim.api.nvim_create_autocmd('BufEnter', {
+  callback = function()
+    local bufnr = vim.api.nvim_get_current_buf()
+    vim.api.nvim_buf_set_var(bufnr, 'cached_formatters', _formatters())
+    vim.api.nvim_buf_set_var(bufnr, 'cached_file', _file())
+  end,
+})
 
 --- builts a statusline section, with its segments separated by the separator
 ---@alias Direction 'left' | 'right'
@@ -106,30 +123,14 @@ end
 function _G.MyStatusLine()
   local branch = _git_branch()
   local git_status = _git_status()
-  local file = _current_file()
-  local lsp = vim.b.cached_lsps or compute_lsps()
-  local formatters = vim.b.cached_formatters or compute_formatters()
+  local file = vim.b.cached_file or _file()
+  local lsp = vim.b.cached_lsps or _lsps()
+  local formatters = vim.b.cached_formatters or _formatters()
 
   local left = _build_section({ branch, file .. git_status }, 'left')
   local right = _build_section({ formatters, lsp }, 'right')
 
   return left .. '%=' .. right
 end
-
--- Cache LSP clients on attach/detach
-vim.api.nvim_create_autocmd({ 'LspAttach', 'LspDetach' }, {
-  callback = function(args)
-    local bufnr = args.buf
-    vim.api.nvim_buf_set_var(bufnr, 'cached_lsps', compute_lsps(bufnr))
-  end,
-})
-
--- Cache formatters on buffer enter
-vim.api.nvim_create_autocmd('BufEnter', {
-  callback = function()
-    local bufnr = vim.api.nvim_get_current_buf()
-    vim.api.nvim_buf_set_var(bufnr, 'cached_formatters', compute_formatters(bufnr))
-  end,
-})
 
 vim.opt.statusline = '%!v:lua.MyStatusLine()'
