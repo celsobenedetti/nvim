3df7a023762211eb8aef1ec1d0ba2893e38d6946
Celso Benedetti <celsobenedetti2@gmail.com>
2025-12-31 16:31:37 -0300

    fix(pairs): ignore space on orgmode tasks

--- lua/modules/editor/pairs.lua
+++ lua/modules/editor/pairs.lua
@@ -24,8 +24,110 @@ return {
         -- and/or with `vim.g.blink_pairs = false` and `vim.b.blink_pairs = false`
         disabled_filetypes = {},
         -- see the defaults:
-        -- https://github.com/Saghen/blink.pairs/blob/main/lua/blink/pairs/config/mappings.lua#L14
-        pairs = {},
+        -- https://github.com/Saghen/blink.pairs/blob/main/lua/blink/pairs/config/mappings.lua
+        -- pairs = {},
+        pairs = {
+          ['!'] = { { '<!--', '-->', languages = { 'html', 'markdown', 'markdown_inline' } } },
+          ['('] = ')',
+          ['['] = {
+            {
+              '[',
+              ']',
+              space = function()
+                return vim.bo.filetype ~= 'markdown' and vim.bo.filetype ~= 'org'
+              end,
+            },
+          },
+          ['{'] = '}',
+          ["'"] = {
+            {
+              "'''",
+              when = function(ctx)
+                return ctx:text_before_cursor(2) == "''"
+              end,
+              languages = { 'python' },
+            },
+            {
+              "'",
+              enter = false,
+              space = false,
+              when = function(ctx)
+                -- The `plaintex` filetype has no treesitter parser, so we can't disable
+                -- this pair in math environments. Thus, disable this pair completely.
+                -- TODO: disable inside "" strings?
+                return ctx.ft ~= 'plaintext'
+                  and ctx.ft ~= 'scheme'
+                  and (not ctx.char_under_cursor:match('%w') or ctx:is_after_cursor("'"))
+                  and ctx.ts:blacklist('singlequote').matches
+              end,
+            },
+          },
+          ['"'] = {
+            {
+              'r#"',
+              '"#',
+              languages = { 'rust' },
+              priority = 100,
+            },
+            {
+              '"""',
+              when = function(ctx)
+                return ctx:text_before_cursor(2) == '""'
+              end,
+              languages = { 'python', 'elixir', 'julia', 'kotlin', 'scala' },
+            },
+            { '"', enter = false, space = false },
+          },
+          ['`'] = {
+            {
+              '```',
+              when = function(ctx)
+                return ctx:text_before_cursor(2) == '``'
+              end,
+              languages = { 'markdown', 'markdown_inline', 'typst', 'vimwiki', 'rmarkdown', 'rmd', 'quarto' },
+            },
+            {
+              '`',
+              "'",
+              languages = { 'bibtex', 'latex', 'plaintex' },
+            },
+            { '`', enter = false, space = false },
+          },
+          ['_'] = {
+            {
+              '_',
+              when = function(ctx)
+                return not ctx.char_under_cursor:match('%w') and ctx.ts:blacklist('underscore').matches
+              end,
+              languages = { 'typst' },
+            },
+          },
+          ['*'] = {
+            {
+              '*',
+              when = function(ctx)
+                return ctx.ts:blacklist('asterisk').matches
+              end,
+              languages = { 'typst' },
+            },
+          },
+          ['<'] = {
+            {
+              '<',
+              '>',
+              when = function(ctx)
+                return ctx.ts:whitelist('angle').matches
+              end,
+              languages = { 'rust' },
+            },
+          },
+          ['$'] = {
+            {
+              '$',
+              languages = { 'markdown', 'markdown_inline', 'typst', 'latex', 'plaintex' },
+            },
+          },
+        },
       },
       highlights = {
         enabled = true,
