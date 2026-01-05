8a39b1fc487fd75d00a4f086f58b199e35ddd659
Celso Benedetti <celsobenedetti2@gmail.com>
2026-01-05 11:14:18 -0300

    feat: add mason-tool-installer

--- lua/modules/core/mason.lua
+++ lua/modules/core/mason.lua
@@ -1,18 +1,35 @@
 return {
-  'mason-org/mason.nvim',
-  lazy = false,
-  keys = { { '<leader>ma', ':Mason<CR>', { desc = 'Mason' } } },
-  cmd = 'Mason',
-  opts = {
-    ensure_installed = {
-      'scheckmake',
-      'eslint-lsp',
-      'json-lsp',
-      'ols',
-      'shellcheck',
-      'shfmt',
-      'trivy',
-      'vue-language-server',
-    },
+  {
+    'mason-org/mason.nvim',
+    lazy = false,
+    keys = { { '<leader>ma', ':Mason<CR>', { desc = 'Mason' } } },
+    cmd = 'Mason',
+    config = true,
+  },
+  {
+    'WhoIsSethDaniel/mason-tool-installer.nvim',
+    config = function()
+      require('mason-tool-installer').setup({
+        ensure_installed = {
+          'lua-language-server',
+          'eslint-lsp',
+          'json-lsp',
+          'ols',
+          'shellcheck',
+          'shfmt',
+          'stylua',
+          'vue-language-server',
+        },
+        auto_update = false,
+        run_on_start = false,
+        start_delay = 3000, -- 3 second delay
+        debounce_hours = 5, -- at least 5 hours between attempts to install/update
+        integrations = {
+          ['mason-lspconfig'] = false,
+          ['mason-null-ls'] = false,
+          ['mason-nvim-dap'] = false,
+        },
+      })
+    end,
   },
 }
