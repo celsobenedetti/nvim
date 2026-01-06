b41df173793d8d00d69c70c570cd3e5b065b88de
Celso Benedetti <celsobenedetti2@gmail.com>
2026-01-05 21:00:37 -0300

    add: compile-mode.nvim

---
+++ lua/modules/editor/compile.lua
@@ -0,0 +1,24 @@
+-- plugin which emulates the features of Emacs' Compilation Mode.
+-- It allows you to run commands which are output into a special buffer,
+-- and then rerun that command over and over again as much as you need.
+-- https://www.gnu.org/software/emacs/manual/html_node/emacs/Compilation-Mode.html
+return {
+  'ej-shafran/compile-mode.nvim',
+  version = '^5.0.0',
+  event = 'CmdlineEnter',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    { 'm00qek/baleia.nvim', tag = 'v1.3.0' }, -- if you want to enable coloring of ANSI escape codes in compilation output
+  },
+  config = function()
+    vim.g.compile_mode = {
+      input_word_completion = true, -- blink.nvim tab completion in command mode:
+      baleia_setup = true, --  ANSI escape code support, add:
+      bang_expansion = true, --  replace special characters (e.g. `%`) in the command (and behave more like `:!`), add:
+    }
+
+    -- replace "!" with "Compile" in command mode
+    -- :help abbreviation
+    vim.cmd('cnoreabbrev ! Compile')
+  end,
+}
