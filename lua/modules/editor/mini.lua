7d0b6a18b1022a5691722d0b7ff5fb5aa6d184e6
Celso Benedetti <celsobenedetti2@gmail.com>
2026-01-05 21:48:56 -0300

    add: mini.pairs, remove buggy blink.pairs

--- lua/modules/editor/mini.lua
+++ lua/modules/editor/mini.lua
@@ -29,6 +29,35 @@ return {
   },
 
   {
+    'nvim-mini/mini.pairs',
+    version = false,
+    config = function()
+      require('mini.pairs').setup({
+        modes = { insert = true, command = true, terminal = true },
+
+        -- Global mappings. Each right hand side should be a pair information, a
+        -- table with at least these fields (see more in |MiniPairs.map|):
+        -- - <action> - one of 'open', 'close', 'closeopen'.
+        -- - <pair> - two character string for pair to be used.
+        -- By default pair is not inserted after `\`, quotes are not recognized by
+        -- <CR>, `'` does not insert pair after a letter.
+        -- Only parts of tables can be tweaked (others will use these defaults).
+        mappings = {
+          ['('] = { action = 'open', pair = '()', neigh_pattern = '[^\\].' },
+          ['['] = { action = 'open', pair = '[]', neigh_pattern = '[^\\].' },
+          ['{'] = { action = 'open', pair = '{}', neigh_pattern = '[^\\].' },
+          [')'] = { action = 'close', pair = '()', neigh_pattern = '[^\\].' },
+          [']'] = { action = 'close', pair = '[]', neigh_pattern = '[^\\].' },
+          ['}'] = { action = 'close', pair = '{}', neigh_pattern = '[^\\].' },
+          ['"'] = { action = 'closeopen', pair = '""', neigh_pattern = '[\n()]', register = { cr = false } },
+          ["'"] = { action = 'closeopen', pair = "''", neigh_pattern = '[\n()]', register = { cr = false } },
+          ['`'] = { action = 'closeopen', pair = '``', neigh_pattern = '[\n()]', register = { cr = false } },
+        },
+      })
+    end,
+  },
+
+  {
     'nvim-mini/mini.snippets',
     version = false,
     config = function()
