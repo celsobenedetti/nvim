dab80f35cd7736866c400ff1edb2eb4b7cecbc38
Celso Benedetti <celsobenedetti2@gmail.com>
2026-01-06 09:43:56 -0300

    replace: mini.pairs -> nvim-autopairs
    getting annoying <CR> issues in insert mode

--- lua/lib/notes.lua
+++ lua/lib/notes.lua
@@ -2,12 +2,12 @@ local M = {}
 
 local TAB_NAME = 'notes'
 
---- @param fn function to be called after tab is created or focus
+--- @param fn function? to be called after tab is created or focus
 M.focus_or_create_notes_tab = function(fn)
   local tab_id = -1
-  local tabs = vim.api.nvim_list_tabpages()
   local has_tabby, tab_name = pcall(require, 'tabby.feature.tab_name')
   if has_tabby then
+    local tabs = vim.api.nvim_list_tabpages()
     for _, tab in ipairs(tabs) do
       if tab_name.get(tab):find(TAB_NAME) then
         tab_id = tab
@@ -20,8 +20,7 @@ M.focus_or_create_notes_tab = function(fn)
     vim.g.fn.rename_tab(vim.g.icons.notes .. TAB_NAME)
     vim.cmd('lcd ' .. vim.g.env.notes.NOTES)
   else
-    local win_id = vim.api.nvim_tabpage_get_win(tab_id)
-    vim.api.nvim_set_current_win(win_id)
+    vim.api.nvim_set_current_win(vim.api.nvim_tabpage_get_win(tab_id))
   end
 
   if fn then
