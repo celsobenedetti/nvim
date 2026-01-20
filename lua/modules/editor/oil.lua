269a881bccc97e91b08f666e31330ea0d7bb6b83
Celso Benedetti <celsobenedetti2@gmail.com>
2026-01-20 16:13:44 -0300

    fix: oil keymaps

--- lua/modules/editor/oil.lua
+++ lua/modules/editor/oil.lua
@@ -2,7 +2,14 @@ return {
   'stevearc/oil.nvim',
   ---@module 'oil'
   ---@type oil.SetupOpts
-  opts = {},
+  opts = {
+    keymaps = {
+      ['<C-h>'] = false,
+      ['<C-l>'] = false,
+      ['<C-p>'] = false,
+      ['gp'] = 'actions.preview',
+    },
+  },
   dependencies = { { 'nvim-mini/mini.icons', opts = {} } },
   lazy = false, -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
   keys = {
