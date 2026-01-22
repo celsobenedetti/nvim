4bcf907f0c7cb3bf0bfa0d0ad42c620608ae1070
Celso Benedetti <celsobenedetti2@gmail.com>
2026-01-22 11:59:28 -0300

    feat: mini % whole buffer motion

--- lua/modules/core/mini.lua
+++ lua/modules/core/mini.lua
@@ -23,6 +23,14 @@ return {
           -- g = LazyVim.mini.ai_buffer, -- buffer
           u = ai.gen_spec.function_call(), -- u for "Usage"
           U = ai.gen_spec.function_call({ name_pattern = '[%w_]' }), -- without dot in function name
+          ['%'] = function()
+            local from = { line = 1, col = 1 }
+            local to = {
+              line = vim.fn.line('$'),
+              col = math.max(vim.fn.getline('$'):len(), 1),
+            }
+            return { from = from, to = to }
+          end,
         },
       })
     end,
