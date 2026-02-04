2b393cd21e7578038ea33c1fac6d1d4ab5a94f4d
Celso Benedetti <celsobenedetti2@gmail.com>
2026-02-04 14:50:36 -0300

    fix: autopairs should ignore next {

--- lua/modules/editor/pairs.lua
+++ lua/modules/editor/pairs.lua
@@ -3,6 +3,8 @@ return {
   event = 'InsertEnter',
   config = function()
     require('nvim-autopairs').setup({
+      -- Ignores word characters, %, ', [, ", ., `, and {
+      ignored_next_char = [=[[%w%%%'%[%"%.%`%${]]=],
       -- BUG: adding this because of broken orgmode integration
       map_cr = false,
     })
