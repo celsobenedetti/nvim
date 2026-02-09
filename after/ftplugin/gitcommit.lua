617c9ffa52c1148cfcd771c301702b2edd0e383f
Celso Benedetti <celsobenedetti2@gmail.com>
2026-02-08 22:33:42 -0300

    ftplugin(gitcommit): <leader>ok -> cancel commit

--- after/ftplugin/gitcommit.lua
+++ after/ftplugin/gitcommit.lua
@@ -1,2 +1,10 @@
+vim.api.nvim_buf_set_keymap(
+  0,
+  'n',
+  '<leader>ok',
+  ':lua vim.cmd("normal! ggdd");vim.cmd("wq")<CR>',
+  { desc = 'gitcommit: cancel commit' }
+)
+
 -- Insert mode when entering git commit
 vim.api.nvim_feedkeys(Keys('i<BS>'), 'n', true)
