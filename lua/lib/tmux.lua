f8ecf7de3ac4f5472bcbafc1bf60ddd4d21e2fa6
Celso Benedetti <celsobenedetti2@gmail.com>
2026-02-08 22:57:30 -0300

    fix(lib.tmux): neww does not need to wrap cmd in quotes

--- lua/lib/tmux.lua
+++ lua/lib/tmux.lua
@@ -3,7 +3,7 @@ local M = {}
 --- launches new window with command
 ---@param cmd string
 M.neww = function(cmd)
-  vim.cmd('silent! !tmux neww  "' .. cmd .. '"')
+  vim.cmd('silent! !tmux neww  ' .. cmd .. '')
 end
 
 return M
