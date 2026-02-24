308b41ee292a38b74fcd14c4431c35778b5afdef
Celso Benedetti <celsobenedetti2@gmail.com>
2026-02-24 11:00:03 -0300

    config(incline): ignore buftypes

--- lua/modules/ui/incline.lua
+++ lua/modules/ui/incline.lua
@@ -4,14 +4,10 @@ return {
     enabled = vim.g.incline,
     dependencies = { { 'nvim-mini/mini.icons', config = true } },
     config = function()
-      local is_notes = require('lib.cwd').matches({ 'notes' })
-      -- if is_notes then
       -- set winbar to render empty line above first line of file
       -- this is done mostly because of obsidian notes, which don't contain a top level H1 by default
       vim.opt.winbar = ' '
-      -- end
 
-      -- local helpers = require('incline.helpers')
       local devicons = require('nvim-web-devicons')
       require('incline').setup({
         window = {
@@ -26,6 +22,22 @@ return {
             winbar = true,
           },
         },
+        ignore = {
+          buftypes = {
+            -- '',
+            'acwrite',
+            -- -- 'help',
+            'nofile',
+            'nowrite',
+            'quickfix',
+            'terminal',
+            'prompt',
+          },
+          filetypes = {},
+          floating_wins = true,
+          unlisted_buffers = false,
+          wintypes = 'special',
+        },
         render = function(props)
           local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ':t')
           if filename == '' then
@@ -34,18 +46,14 @@ return {
           local ft_icon, ft_color = devicons.get_icon_color(filename)
           local modified = vim.bo[props.buf].modified
           return {
-            ft_icon
-                and {
-                  ' ',
-                  ft_icon,
-                  ' ',
-                  guifg = ft_color,
-                  -- guibg = helpers.contrast_color(ft_color),
-                }
-              or '',
+            ft_icon and {
+              ' ',
+              ft_icon,
+              ' ',
+              guifg = ft_color,
+            } or '',
             { filename, gui = modified and 'bold,italic' or 'bold' },
             ' ',
-            -- guibg = '#44406e',
           }
         end,
       })
