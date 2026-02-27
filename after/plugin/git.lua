652427707f5effa87ce518444a4b35c4e4a42f8d
Celso Benedetti <celsobenedetti2@gmail.com>
2026-02-27 15:22:15 -0300

    tweak(gp): use Git push instead of Compile git push

--- after/plugin/git.lua
+++ after/plugin/git.lua
@@ -1,6 +1,6 @@
 -- stylua: ignore start
 map('n', 'gs', function() Snacks.picker.git_status({ layout = 'ivy_split' }) end, { desc = 'git: (snacks) git Status' })
-map('n', 'gp', ':Compile git push<CR>', { desc = 'git: push' })
+map('n', 'gp', ':Git push<CR>', { desc = 'git: push' })
 map('n', 'gC', function() vim.cmd('Git commit') end, { desc = 'git: Git commit' })
 map('n', 'gA', function() vim.cmd('Git add -p') end, { desc = 'git: Git add -p`', })
 map('n', 'gR', function() vim.cmd("Git restore -p") end, { desc = 'git: Git restore -p' })
