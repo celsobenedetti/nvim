c93312e94f953835f856b7e80b780cf450651191
Celso Benedetti <celsobenedetti2@gmail.com>
2026-03-02 16:01:04 -0300

    fix(gC): ammend if nothing is staged

--- after/plugin/git.lua
+++ after/plugin/git.lua
@@ -1,11 +1,20 @@
 -- stylua: ignore start
 map('n', 'gs', function() Snacks.picker.git_status({ layout = 'ivy_split' }) end, { desc = 'git: (snacks) git Status' })
 map('n', 'gp', ':Git push<CR>', { desc = 'git: push' })
-map('n', 'gC', function() vim.cmd('Git commit') end, { desc = 'git: Git commit' })
 map('n', 'gA', function() vim.cmd('Git add -p') end, { desc = 'git: Git add -p`', })
 map('n', 'gR', function() vim.cmd("Git restore -p") end, { desc = 'git: Git restore -p' })
 map('n', '<leader>gd', function() vim.cmd("CodeDiff") end, { desc = 'git: (codediff) git diff' })
 -- stylua: ignore end
+
+map('n', 'gC', function()
+  local result = vim.system({ 'git', 'diff', '--staged', '--name-only' }):wait()
+  local has_staged = result.stdout ~= nil and result.stdout ~= ''
+  if has_staged then
+    vim.cmd('Git commit')
+  else
+    vim.cmd('Git commit --amend')
+  end
+end, { desc = 'git: Git commit (or amend if nothing staged)' })
 
 map('n', 'ga', function()
   local file = vim.fn.expand('%')
