33231d77a1267b82b366b326c852aff65db15abe
Celso Benedetti <celsobenedetti2@gmail.com>
2026-03-24 08:41:20 -0300

    ui: deepwhite

--- lua/modules/ui/deepwhite.lua
+++ lua/modules/ui/deepwhite.lua
@@ -10,9 +10,22 @@ return {
     config = function()
       require('deepwhite').setup({})
 
-      vim.schedule(function()
-        local colors = require('deepwhite.colors').get_colors({})
+      local colors = vim.g.colors
+      colors.red = colors.color1
+      colors.green = colors.color2
+      colors.orange = colors.color3
+      colors.light_orange = '#FAE1C8' -- hsv(30, 20%, 98%)
+      colors.light_yellow = '#FAFAC8' -- hsv(60, 20%, 98%)
+      colors.light_cyan = '#C8FAFA' -- hsv(180, 20%, 98%)
+      colors.light_green = '#D4FAD4' -- hsv(120, 15%, 98%)
+      colors.light_blue = '#D4D4FA' -- hsv(240, 15%, 98%)
+      colors.light_purple = '#EDD4FA' -- hsv(280, 15%, 98%)
+      colors.light_pink = '#FAD4ED' -- hsv(320, 15%, 98%)
+      colors.light_red = '#FAD4D4' -- hsv(360, 15%, 98%)
+      colors.secondary = '#595855'
+      vim.g.colors = colors
 
+      vim.schedule(function()
         vim.api.nvim_set_hl(0, 'ColorColumn', { bg = 'None' })
         vim.api.nvim_set_hl(0, 'Constant', { bg = 'None' })
         vim.api.nvim_set_hl(0, 'String', { fg = colors['green'] })
