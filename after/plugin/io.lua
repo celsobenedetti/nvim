3233a2d0c0808699e0a57e7b67196f291e007353
Celso Benedetti <celsobenedetti2@gmail.com>
2026-05-21 18:36:59 -0300

    feat(io): crawler microservices

--- after/plugin/io.lua
+++ after/plugin/io.lua
@@ -1,3 +1,12 @@
-if not require('lib.cwd').matches({ vim.g.dirs.work.io }) then
+if not require('lib.cwd').matches({ 'work/io' }) then
   return
 end
+
+vim.api.nvim_create_user_command('IoCrawlers', function()
+  require('lib.overseer.run_tasks').run({
+    { name = 'crawler-diff', cmd = 'npm', args = { 'run', 'devms', 'crawler-diff' } },
+    { name = 'wforge-qans-indexer', cmd = 'npm', args = { 'run', 'devms', 'wforge-qans-indexer' } },
+    { name = 'wforge-orchestrator', cmd = 'npm', args = { 'run', 'devms', 'wforge-orchestrator' } },
+    { name = 'bot-clearer', cmd = 'npm', args = { 'run', 'devms', 'bot-clearer' } },
+  })
+end, { desc = 'Run all crawler tasks' })
