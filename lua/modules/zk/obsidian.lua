025bec10c1c4bd96ca684079d69d440d22e2313e
Celso Benedetti <celsobenedetti2@gmail.com>
2026-05-28 18:21:59 -0300

    feat(md): <leader>md toggles UI

--- lua/modules/zk/obsidian.lua
+++ lua/modules/zk/obsidian.lua
@@ -86,6 +86,27 @@ return {
         end,
         desc = 'Grep through notes',
       },
+      {
+        '<leader>md',
+        function()
+          local ui_opts = Obsidian and Obsidian.opts.ui
+          if not ui_opts then
+            return
+          end
+          ui_opts.enable = not ui_opts.enable
+          local ns = vim.api.nvim_create_namespace('ObsidianUI')
+          if ui_opts.enable then
+            require('obsidian.ui').update()
+          else
+            for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+              if vim.api.nvim_buf_is_valid(buf) then
+                vim.api.nvim_buf_clear_namespace(buf, ns, 0, -1)
+              end
+            end
+          end
+        end,
+        desc = 'Toggle Obsidian UI',
+      },
       { '<leader>oO', '<cmd>Obsidian open<CR>', desc = 'Open in Obsidian' },
       { '<leader>ob', '<cmd>Obsidian backlinks<CR>', desc = 'Backlinks' },
       { '<leader>od', '<cmd>Obsidian dailies<CR>', desc = 'Daily notes' },
