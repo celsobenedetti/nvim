61043a7b30b26c8d902592da610927c693209e33
Celso Benedetti <celsobenedetti2@gmail.com>
2026-05-28 14:36:39 -0300

    add: filetype plugin

---
+++ after/plugin/filetype.lua
@@ -0,0 +1,9 @@
+-- https://github.com/gennaro-tedesco/dotfiles/blob/28be096a90a7c1fbadde62bdac3fd2a78492fcde/nvim/lua/filetype.lua#L7
+vim.filetype.add({
+  filename = {
+    ['.env'] = 'config',
+  },
+  pattern = {
+    ['gitconf.*'] = 'gitconfig',
+  },
+})
