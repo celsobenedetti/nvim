de9034872fb9203a2499ef2f8d0febcb5cdbd78c
Celso Benedetti <celsobenedetti2@gmail.com>
2026-06-04 20:25:42 -0300

    fix: dropbar enable check

--- lua/modules/ui/dropbar.lua
+++ lua/modules/ui/dropbar.lua
@@ -1,10 +1,22 @@
 return {
   {
     'Bekaboo/dropbar.nvim',
-    event = 'VeryLazy',
     enabled = vim.g.dropbar,
     opts = function(_, opts)
       opts.bar = {
+        enable = function(buf, win, _)
+          if not vim.api.nvim_buf_is_valid(buf) or not vim.api.nvim_win_is_valid(win) then
+            return false
+          end
+          if vim.fn.win_gettype(win) ~= '' or vim.bo[buf].ft == 'help' then
+            return false
+          end
+          local stat = vim.uv.fs_stat(vim.api.nvim_buf_get_name(buf))
+          if stat and stat.size > 1024 * 1024 then
+            return false
+          end
+          return vim.bo[buf].bt == 'terminal' or true
+        end,
         sources = function(buf, _)
           local sources = require('dropbar.sources')
           local utils = require('dropbar.utils')
