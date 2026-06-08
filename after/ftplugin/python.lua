67a7a9af347dca60949146bf963710139614ad33
Celso Benedetti <celsobenedetti2@gmail.com>
2026-06-08 16:29:42 -0300

    fix(py): foldmethod=expr

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -1,11 +1,1 @@
-local group = vim.api.nvim_create_augroup('python_ftplugin', { clear = true })
-vim.api.nvim_create_autocmd('BufReadPost', {
-  desc = 'python: run only once when buffer is loaded',
-  group = group,
-  pattern = '*.py',
-  callback = function()
-    vim.wo.foldmethod = 'expr'
-    vim.wo.foldlevel = 1
-    vim.wo.foldnestmax = 2
-  end,
-})
+vim.wo.foldlevel = 0
