9f1ca1ea55d0d76384347a326266921c0d89f51a
Celso Benedetti <celsobenedetti2@gmail.com>
2026-06-09 22:33:24 -0300

    commitas

--- lua/modules/overseer/overseer.lua
+++ lua/modules/overseer/overseer.lua
@@ -64,43 +64,4 @@ return {
       { "<leader>OC", "<cmd>OverseerClearCache<cr>",  desc = "Overseer: Clear cache" },
     },
   },
-  -- {
-  --   'folke/which-key.nvim',
-  --   optional = true,
-  --   opts = {
-  --     spec = {
-  --       { '<leader>o', group = 'overseer' },
-  --     },
-  --   },
-  -- },
-  -- {
-  --   'folke/edgy.nvim',
-  --   optional = true,
-  --   opts = function(_, opts)
-  --     opts.right = opts.right or {}
-  --     table.insert(opts.right, {
-  --       title = 'Overseer',
-  --       ft = 'OverseerList',
-  --       open = function()
-  --         require('overseer').open()
-  --       end,
-  --     })
-  --   end,
-  -- },
-  -- {
-  --   'nvim-neotest/neotest',
-  --   optional = true,
-  --   opts = function(_, opts)
-  --     opts = opts or {}
-  --     opts.consumers = opts.consumers or {}
-  --     opts.consumers.overseer = require 'neotest.consumers.overseer'
-  --   end,
-  -- },
-  -- {
-  --   'mfussenegger/nvim-dap',
-  --   optional = true,
-  --   opts = function()
-  --     require('overseer').enable_dap()
-  --   end,
-  -- },
 }
