445503f9cc7bd68acaf146542f18f6835dd6943d
Celso Benedetti <celsobenedetti2@gmail.com>
2026-06-19 17:19:04 -0300

    commitas

--- lua/modules/zk/obsidian.lua
+++ lua/modules/zk/obsidian.lua
@@ -93,22 +93,32 @@ return {
       {
         '<leader>md',
         function()
-          local ui_opts = Obsidian and Obsidian.opts and Obsidian.opts.ui
-          if not ui_opts then
-            return
-          end
-          ui_opts.enable = not ui_opts.enable
-          if ui_opts.enable then
-            require('obsidian.ui').update()
-          else
+          -- local ui_opts = Obsidian and Obsidian.opts and Obsidian.opts.ui
+          -- if not ui_opts then
+          --   return
+          -- end
+          -- ui_opts.enable = not ui_opts.enable
+          -- if ui_opts.enable then
+          --   require('obsidian.ui').update()
+          -- else
+          --   local ns = vim.api.nvim_create_namespace('ObsidianUI')
+          --   for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+          --     if vim.api.nvim_buf_is_valid(buf) then
+          --       vim.api.nvim_buf_clear_namespace(buf, ns, 0, -1)
+          --     end
+          --   end
+          -- end
+
+          local enabled = Obsidian.opts.ui.enable
+          Obsidian.opts.ui.enable = not enabled
+          if enabled then
             local ns = vim.api.nvim_create_namespace('ObsidianUI')
-            for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-              if vim.api.nvim_buf_is_valid(buf) then
-                vim.api.nvim_buf_clear_namespace(buf, ns, 0, -1)
-              end
-            end
+            vim.api.nvim_buf_clear_namespace(0, ns, 0, -1)
+          else
+            require('obsidian.ui').update(0)
           end
         end,
+
         desc = 'Toggle Obsidian UI',
       },
       { '<leader>oO', '<cmd>Obsidian open<CR>', desc = 'Open in Obsidian' },
