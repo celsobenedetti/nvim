fa6b3a301811e55ed99c1a6ac9a31815c448d848
Celso Benedetti <celsobenedetti2@gmail.com>
2026-08-17 21:25:30 -0300

    remove: dead lib.utils

--- lua/lib/utils.lua
+++
@@ -1,9 +0,0 @@
----@class LibUtils
-local M = {}
-
-M.statusline = function()
-  local branch = vim.trim(vim.fn.system('git branch --show-current'))
-  return branch .. ' ' .. vim.fn.expand('%')
-end
-
-return M
