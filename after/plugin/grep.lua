5be9a18b74fbe55f1672710920ee339f48f0923e
Celso Benedetti <celsobenedetti2@gmail.com>
2026-08-19 13:26:04 -0300

    feat(grep): support target arg

--- after/plugin/grep.lua
+++ after/plugin/grep.lua
@@ -3,16 +3,79 @@
 vim.opt.grepprg = 'rg --vimgrep --smart-case --hidden --ignore'
 vim.opt.grepformat = '%f:%l:%c:%m'
 
-vim.api.nvim_create_user_command('Grep', function(opts)
-  local pattern = opts.args
-  if not pattern then
-    vim.ui.input({ prompt = 'Grep: ' }, function(_pattern)
-      pattern = _pattern
-    end)
+-- Split args into tokens on whitespace, honoring "..." and '...' quoting and
+-- backslash escapes. This lets `:Grep "some query" file.txt` parse the quoted
+-- pattern as a single token, unlike a dumb split on spaces.
+local function split_args(args)
+  local result, current, in_arg, quote = {}, '', false, nil
+  local i, n = 1, #args
+  while i <= n do
+    local c = args:sub(i, i)
+    if c == '\\' then
+      local nxt = args:sub(i + 1, i + 1)
+      if nxt ~= '' then
+        current = current .. nxt
+        i = i + 2
+      else
+        current = current .. c
+        i = i + 1
+      end
+      in_arg = true
+    elseif quote then
+      if c == quote then
+        quote = nil
+      else
+        current = current .. c
+      end
+      i = i + 1
+    elseif c == '"' or c == "'" then
+      quote = c
+      in_arg = true
+      i = i + 1
+    elseif c == ' ' or c == '\t' then
+      if in_arg then
+        result[#result + 1] = current
+        current, in_arg = '', false
+      end
+      i = i + 1
+    else
+      current = current .. c
+      in_arg = true
+      i = i + 1
+    end
   end
-  if not pattern then
+  if in_arg then
+    result[#result + 1] = current
+  end
+  return result
+end
+
+local function do_grep(pattern, target)
+  local cmd = 'silent grep ' .. vim.fn.fnameescape(pattern)
+  if target then
+    cmd = cmd .. ' ' .. vim.fn.fnameescape(target)
+  end
+  vim.cmd(cmd)
+  vim.cmd.copen()
+end
+
+vim.api.nvim_create_user_command('Grep', function(opts)
+  local args = split_args(opts.args)
+  if #args == 0 then
+    vim.ui.input({ prompt = 'Grep: ' }, function(input)
+      if input and input ~= '' then
+        do_grep(input)
+      end
+    end)
     return
   end
-  vim.cmd('silent grep ' .. vim.fn.fnameescape(pattern))
-  vim.cmd.copen()
-end, { nargs = '?' })
+  if #args > 2 then
+    vim.notify('Usage: :Grep [pattern] [target]', vim.log.levels.WARN)
+    return
+  end
+  if args[1] == '' then
+    vim.notify('Grep: empty pattern', vim.log.levels.WARN)
+    return
+  end
+  do_grep(args[1], args[2])
+end, { nargs = '*', desc = 'rg grep; optional second arg scopes to a file or dir' })
